ff592220fe78e8b25ab2e2214dbad84940be94d7
FusionPBX <markjcrane@gmail.com>
2016-05-04 17:13:09 -0600

    Update package-master.sh
    Add file permissions for the freeswitch master  package

--- debian/resources/switch/package-master.sh
+++ debian/resources/switch/package-master.sh
@@ -6,3 +6,23 @@ echo "deb http://files.freeswitch.org/repo/deb/debian-unstable/ jessie main" >> 
 apt-get update && apt-get install -y --force-yes freeswitch-all freeswitch-all-dbg gdb
 #apt-get remove freeswitch-all freeswitch-all-dbg
 #rm /etc/apt/sources.list.d/freeswitch.list
+
+#set the file permissions
+chown -R freeswitch:freeswitch /var/lib/freeswitch
+chmod -R g+s /var/lib/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
+
+chown -R freeswitch:freeswitch /usr/share/freeswitch
+chmod -R g+s /usr/share/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
+
+chown -R freeswitch:freeswitch /etc/freeswitch
+chmod -R g+s /etc/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
+
+chown -R freeswitch:freeswitch /var/log/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
+
+chown -R freeswitch:freeswitch /usr/share/freeswitch/sounds
+chmod -R g+s /usr/share/freeswitch/sounds
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
