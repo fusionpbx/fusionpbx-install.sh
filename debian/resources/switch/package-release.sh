82ecab168b7ab54f2d7c8a236734e34bec8a9f9b
FusionPBX <markjcrane@gmail.com>
2016-05-04 14:49:47 -0600

    Update package-release.sh
    Add -d option to setfacl for default.

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -9,19 +9,19 @@ apt-get update && apt-get install -y --force-yes freeswitch-all freeswitch-all-d
 #set the file permissions
 #chown -R freeswitch:freeswitch /var/lib/freeswitch
 chmod -R g+s /var/lib/freeswitch
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
 
 #chown -R freeswitch:freeswitch /usr/share/freeswitch
 chmod -R g+s /usr/share/freeswitch
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
 
 #chown -R freeswitch:freeswitch /etc/freeswitch
 chmod -R g+s /etc/freeswitch
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
 
 #chown -R freeswitch:freeswitch /etc/freeswitch
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
 
 #chown -R freeswitch:freeswitch /usr/share/freeswitch/sounds
 chmod -R g+s /usr/share/freeswitch/sounds
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
+setfacl -d -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
