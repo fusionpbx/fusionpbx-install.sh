4cb92462d52b34af1e3238df622c0fff24b8c441
FusionPBX <markjcrane@gmail.com>
2016-05-06 14:57:17 -0600

    Update package-master.sh
    Change the setfacl syntax

--- debian/resources/switch/package-master.sh
+++ debian/resources/switch/package-master.sh
@@ -11,21 +11,21 @@ apt-get update && apt-get install -y --force-yes freeswitch-all freeswitch-all-d
 chown -R freeswitch:freeswitch /var/lib/freeswitch
 chmod -R g+s /var/lib/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
+setfacl -R -m d:u:www-data:rwx,d:g:www-data:rwx /var/lib/freeswitch
 
 chown -R freeswitch:freeswitch /usr/share/freeswitch
 chmod -R g+s /usr/share/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
+setfacl -R -m d:u:www-data:rwx,d:g:www-data:rwx /usr/share/freeswitch
 
 chown -R freeswitch:freeswitch /etc/freeswitch
 chmod -R g+s /etc/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /etc/freeswitch
+setfacl -R -m d:u:www-data:rwx,d:g:www-data:rwx /etc/freeswitch
 
 chown -R freeswitch:freeswitch /var/log/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
+setfacl -R -m d:u:www-data:rwx,d:g:www-data:rwx /var/log/freeswitch
 
 chown -R freeswitch:freeswitch /usr/share/freeswitch/sounds
 chmod -R g+s /usr/share/freeswitch/sounds
