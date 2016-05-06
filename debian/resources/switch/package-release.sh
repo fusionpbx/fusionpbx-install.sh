1cd570ed2bd6cc1f992bb9e65bbc67b6222a79f1
FusionPBX <markjcrane@gmail.com>
2016-05-06 15:25:38 -0600

    Update package-release.sh
    Move the -d before the -m

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -15,18 +15,18 @@ setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
 chown -R freeswitch:freeswitch /usr/share/freeswitch
 chmod -R g+s /usr/share/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
+setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
 
 chown -R freeswitch:freeswitch /etc/freeswitch
 chmod -R g+s /etc/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /etc/freeswitch
+setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
 
 chown -R freeswitch:freeswitch /var/log/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
+setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
 
 chown -R freeswitch:freeswitch /usr/share/freeswitch/sounds
 chmod -R g+s /usr/share/freeswitch/sounds
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
+setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
