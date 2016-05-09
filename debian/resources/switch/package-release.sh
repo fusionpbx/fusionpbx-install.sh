acadeb2783b0b14afab04085798fe657db408045
DigiDaz <DigiDaz@users.noreply.github.com>
2016-05-09 20:24:35 +0100

    Make non-use of repo default and fix last setfacl error
    Make non-use of repo default and fix last setfacl error
    
    USE_UNOFFICIAL_ARM_REPO needs to be set to 1 to use repo.

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 apt-get update && apt-get install -y --force-yes curl memcached haveged
+USE_UNOFFICIAL_ARM_REPO=0
 arch=$(uname -m)
-if [ $arch = 'armv7l' ]; then
+if [ $arch = 'armv7l' ] && [ $USE_UNOFFICIAL_ARM_REPO -eq 1 ]; then
         echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
 else
@@ -16,7 +17,7 @@ apt-get update && apt-get install -y --force-yes freeswitch-all freeswitch-all-d
 chown -R freeswitch:freeswitch /var/lib/freeswitch
 chmod -R g+s /var/lib/freeswitch
 setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
-setfacl -R -m -d u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
+setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
 
 chown -R freeswitch:freeswitch /usr/share/freeswitch
 chmod -R g+s /usr/share/freeswitch
