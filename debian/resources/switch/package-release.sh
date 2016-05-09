dfb0ab299050afb494be3ad069dad22d0647d9f9
DigiDaz <DigiDaz@users.noreply.github.com>
2016-05-09 19:46:16 +0100

    Fixed repo path
    Fix repo paths

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -2,10 +2,10 @@
 apt-get update && apt-get install -y --force-yes curl memcached haveged
 arch=$(uname -m)
 if [ $arch = 'armv7l' ]; then
-        echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" >> /etc/apt/sources.list
+        echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
 else
-        echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list
+        echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://files.freeswitch.org/repo/deb/freeswitch-1.6/key.gpg | apt-key add -
 fi
 apt-get update && apt-get install -y --force-yes freeswitch-all freeswitch-all-dbg gdb
