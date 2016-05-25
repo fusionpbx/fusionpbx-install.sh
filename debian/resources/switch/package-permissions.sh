14d1fb827e989bf627be953efecc7f62fc4abcf4
FusionPBX <markjcrane@gmail.com>
2016-05-25 14:16:52 -0600

    Update package-permissions.sh
    Remove the facl and add +x to the directories to get groups working.
    
    For installs using the facl can use this to remove them.
    
    setfacl --remove-all -R /usr/share/freeswitch
    setfacl --remove-all -R /var/log/freeswitch
    setfacl --remove-all -R /etc/freeswitch
    setfacl --remove-all -R /var/lib/freeswitch

--- debian/resources/switch/package-permissions.sh
+++ debian/resources/switch/package-permissions.sh
@@ -4,35 +4,19 @@ usermod -a -G www-data freeswitch
 
 #setup owner and group, permissions and sticky
 chown -R freeswitch:freeswitch /var/lib/freeswitch
-chmod -R 760 /var/lib/freeswitch
+chmod -R 770 /var/lib/freeswitch
 chmod -R g+s /var/lib/freeswitch
 
 chown -R freeswitch:freeswitch /usr/share/freeswitch
-chmod -R 760 /usr/share/freeswitch
-chmod -R 760 /usr/share/freeswitch/scripts
-chmod -R 760 /usr/share/freeswitch/lang
+chmod -R 770 /usr/share/freeswitch
+#chmod -R 770 /usr/share/freeswitch/scripts
+#chmod -R 770 /usr/share/freeswitch/lang
 chmod -R g+s /usr/share/freeswitch
 
 chown -R freeswitch:freeswitch /etc/freeswitch
-chmod -R 760 /etc/freeswitch
+chmod -R 770 /etc/freeswitch
 chmod -R g+s /etc/freeswitch
 
 chown -R freeswitch:freeswitch /var/log/freeswitch
-chmod -R 760 /var/log/freeswitch
+chmod -R 770 /var/log/freeswitch
 chmod -R g+s /var/log/freeswitch
-
-#file access control lists
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
-setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /var/lib/freeswitch
-
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
-setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch
-
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
-setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /etc/freeswitch
-
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
-setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /var/log/freeswitch
-
-setfacl -R -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
-setfacl -R -d -m u:www-data:rwx,g:www-data:rwx /usr/share/freeswitch/sounds
