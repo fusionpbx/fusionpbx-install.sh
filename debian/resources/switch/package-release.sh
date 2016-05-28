bf5a253f6500d51a30b7ae3f8a2a2a31889de5ad
Mafoo <github@mafoo.org>
2016-05-28 19:09:45 +0100

    fixes for fail2ban (#10)
    export the command line variables so sub scripts can use them
    use sed to update log path is source is used
    simplify freeswitch rules to use protocol=all
    general tidy up of spacing in files

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+#initialize variable encase we are called directly
+[ -z $USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM ] && USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM=false
+
 apt-get update && apt-get install -y --force-yes curl memcached haveged
-USE_UNOFFICIAL_ARM_REPO=0
 arch=$(uname -m)
-if [ $arch = 'armv7l' ] && [ $USE_UNOFFICIAL_ARM_REPO -eq 1 ]; then
+if [ $arch = 'armv7l' ] && [ $USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM = true ]; then
         echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
 else
