98e386458b2349e671a1812d10027cfa4e152e20
markjcrane <markjcrane@gmail.com>
2016-06-04 13:21:32 -0600

    Shorten the parameter names used in the install script. I'm considering shortening them further.

--- debian/install.sh
+++ debian/install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 #Process command line options
-OPTS=`getopt -n 'install.sh' -o h -l help,use-freeswitch-source,use-freeswitch-package-all,use-freeswitch-master,use-freeswitch-package-unofficial-arm -- "$@"`
+OPTS=`getopt -n 'install.sh' -o h -l help,use-switch-source,use-switch-package-all,use-switch-master,use-switch-package-unofficial-arm -- "$@"`
 eval set -- "$OPTS"
 
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 
-export USE_FREESWITCH_SOURCE=false
-export USE_FREESWITCH_PACKAGE_ALL=false
-export USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM=false
-export USE_FREESWITCH_MASTER=false
+export USE_SWITCH_SOURCE=false
+export USE_SWITCH_PACKAGE_ALL=false
+export USE_SWITCH_PACKAGE_UNOFFICIAL_ARM=false
+export USE_SWITCH_MASTER=false
 HELP=false
 
 while true; do
   case "$1" in
-    --use-freeswitch-source ) export USE_FREESWITCH_SOURCE=true; shift ;;
-    --use-freeswitch-package-all ) export USE_FREESWITCH_PACKAGE_ALL=true; shift ;;
-    --use-freeswitch-package-unofficial-arm ) export USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM=true; shift ;;
-    --use-freeswitch-master ) export USE_FREESWITCH_MASTER=true; shift ;;
+    --use-switch-source ) export USE_SWITCH_SOURCE=true; shift ;;
+    --use-switch-package-all ) export USE_SWITCH_PACKAGE_ALL=true; shift ;;
+    --use-switch-package-unofficial-arm ) export USE_SWITCH_PACKAGE_UNOFFICIAL_ARM=true; shift ;;
+    --use-switch-master ) export USE_SWITCH_MASTER=true; shift ;;
     -h | --help ) HELP=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
@@ -26,10 +26,10 @@ done
 
 if [ $HELP = true ]; then
 	echo "Debian installer script"
-	echo "	--use-freeswitch-source will use freeswitch from source rather than (default:packages)"
-	echo "	--use-freeswitch-package-all if using packages use the meta-all package"
-	echo "	--use-freeswitch-package-unofficial-arm if your system is arm and you are using packages, use the unofficial arm repo"
-	echo "	--use-freeswitch-master will use master branch/packages instead of (default:stable)"
+	echo "	--use-switch-source will use freeswitch from source rather than (default:packages)"
+	echo "	--use-switch-package-all if using packages use the meta-all package"
+	echo "	--use-switch-package-unofficial-arm if your system is arm and you are using packages, use the unofficial arm repo"
+	echo "	--use-switch-master will use master branch/packages instead of (default:stable)"
 	exit;
 fi
 
@@ -50,22 +50,22 @@ resources/nginx.sh
 resources/fail2ban.sh
 
 #FreeSWITCH
-if [ $USE_FREESWITCH_SOURCE = true ]; then
-	if [ $USE_FREESWITCH_MASTER = true ]; then
+if [ $USE_SWITCH_SOURCE = true ]; then
+	if [ $USE_SWITCH_MASTER = true ]; then
 		resources/switch/source-master.sh
 	else
 		resources/switch/source-release.sh
 	fi
 	resources/switch/source-permissions.sh
 else
-	if [ $USE_FREESWITCH_MASTER = true ]; then
-		if [ $USE_FREESWITCH_PACKAGE_ALL = true ]; then
+	if [ $USE_SWITCH_MASTER = true ]; then
+		if [ $USE_SWITCH_PACKAGE_ALL = true ]; then
 			resources/switch/package-master-all.sh
 		else
 			resources/switch/package-master.sh
 		fi
 	else
-		if [ $USE_FREESWITCH_PACKAGE_ALL = true ]; then
+		if [ $USE_SWITCH_PACKAGE_ALL = true ]; then
 			resources/switch/package-all.sh
 		else
 			resources/switch/package-release.sh
--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #initialize variable encase we are called directly
-[ -z $USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM ] && USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM=false
+[ -z $USE_SWITCH_PACKAGE_UNOFFICIAL_ARM ] && USE_SWITCH_PACKAGE_UNOFFICIAL_ARM=false
 
 apt-get update && apt-get install -y --force-yes curl memcached haveged
 arch=$(uname -m)
-if [ $arch = 'armv7l' ] && [ $USE_FREESWITCH_PACKAGE_UNOFFICIAL_ARM = true ]; then
+if [ $arch = 'armv7l' ] && [ $USE_SWITCH_PACKAGE_UNOFFICIAL_ARM = true ]; then
         echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
 else
