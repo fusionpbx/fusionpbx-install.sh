7de68ccb84ea8b5d707a0be1e8d6cddb46dff952
markjcrane <markjcrane@gmail.com>
2016-06-08 09:28:47 -0600

    Add postgresql repo and 2ndquadrant.

--- debian/resources/postgres.sh
+++ debian/resources/postgres.sh
@@ -10,6 +10,14 @@ password=$(dd if=/dev/urandom bs=1 count=20 2>/dev/null | base64)
 echo "Install PostgreSQL and create the database and users\n"
 apt-get install -y --force-yes sudo postgresql
 
+#Add PostgreSQL and BDR REPO
+#echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main'  >> /etc/apt/sources.list.d/2ndquadrant.list
+#echo 'deb http://packages.2ndquadrant.com/bdr/apt/ jessie-2ndquadrant main' >> /etc/apt/sources.list.d/2ndquadrant.list
+#/usr/bin/wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+#/usr/bin/wget --quiet -O - http://packages.2ndquadrant.com/bdr/apt/AA7A6805.asc | apt-key add -
+#apt-get update && apt-get upgrade -y
+#apt-get install -y --force-yes postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4
+
 #systemd
 /bin/systemctl daemon-reload
 /bin/systemctl restart postgresql
