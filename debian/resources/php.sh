ace73b168a6a9e1b4e2a3d5afd95c5ff2f2bd031
FusionPBX <markjcrane@gmail.com>
2016-08-03 12:09:53 -0600

    Update php.sh

--- debian/resources/php.sh
+++ debian/resources/php.sh
@@ -4,7 +4,7 @@
 echo "Configure PHP"
 
 #update config if source is being used
-#sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php5/fpm/php.ini
+sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php5/fpm/php.ini
 sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i /etc/php5/fpm/php.ini
 
 #restart php5-fpm
