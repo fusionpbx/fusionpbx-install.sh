df19549c9fe0e97d2d69c3d786b72173e4d16a59
FusionPBX <markjcrane@gmail.com>
2016-08-11 01:02:49 -0600

    Update source-permissions.sh

--- debian/resources/switch/source-permissions.sh
+++ debian/resources/switch/source-permissions.sh
@@ -1,8 +1,4 @@
-#add users to the groups
-usermod -a -G freeswitch www-data
-usermod -a -G www-data freeswitch
-
 #setup owner and group, permissions and sticky
-chown -R freeswitch:freeswitch /usr/local/freeswitch
+chown -R www-data:www-data /usr/local/freeswitch
 chmod -R ug+rw /usr/local/freeswitch
 find /usr/local/freeswitch -type d -exec chmod 2770 {} \;
