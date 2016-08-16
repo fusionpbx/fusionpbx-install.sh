55a56e607eba2945921d6f5364b2cd5c24ed0def
FusionPBX <markjcrane@gmail.com>
2016-08-15 18:50:58 -0600

    Update package-permissions.sh

--- debian/resources/switch/package-permissions.sh
+++ debian/resources/switch/package-permissions.sh
@@ -3,3 +3,4 @@ chown -R www-data:www-data /etc/freeswitch
 chown -R www-data:www-data /var/lib/freeswitch
 chown -R www-data:www-data /usr/share/freeswitch
 chown -R www-data:www-data /var/log/freeswitch
+chown -R www-data:www-data /var/run/freeswitch
