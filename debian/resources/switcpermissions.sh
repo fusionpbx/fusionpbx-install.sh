fcd829b20a303c28c78f3aace5b7bf18395aee0e
FusionPBX <markjcrane@gmail.com>
2016-08-15 22:03:46 -0600

    Update switcpermissions.sh

--- debian/resources/switcpermissions.sh
+++ debian/resources/switcpermissions.sh
@@ -1,4 +1,3 @@
-#setup owner and group, permissions and sticky
 chown -R www-data:www-data /etc/freeswitch
 chown -R www-data:www-data /var/lib/freeswitch
 chown -R www-data:www-data /usr/share/freeswitch
