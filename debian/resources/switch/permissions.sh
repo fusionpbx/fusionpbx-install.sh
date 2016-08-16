19daa2f94fcca67b62859e93d44df8eeb35b81a3
FusionPBX <markjcrane@gmail.com>
2016-08-16 11:58:24 -0600

    Delete permissions.sh

--- debian/resources/switch/permissions.sh
+++
@@ -1,5 +0,0 @@
-chown -R www-data:www-data /etc/freeswitch
-chown -R www-data:www-data /var/lib/freeswitch
-chown -R www-data:www-data /usr/share/freeswitch
-chown -R www-data:www-data /var/log/freeswitch
-chown -R www-data:www-data /var/run/freeswitch
