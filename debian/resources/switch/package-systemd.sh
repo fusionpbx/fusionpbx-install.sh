7c3c2dbebef7e08371fc370fe3cc0775429e836d
FusionPBX <markjcrane@gmail.com>
2016-08-16 15:56:04 -0600

    Update package-systemd.sh

--- debian/resources/switch/package-systemd.sh
+++ debian/resources/switch/package-systemd.sh
@@ -1,3 +1,4 @@
+apt-get remove -y --force-yes freeswitch-systemd
 cp "$(dirname $0)/source/freeswitch.service.package" /lib/systemd/system/freeswitch.service
 cp "$(dirname $0)/source/etc.default.freeswitch" /etc/default/freeswitch
 systemctl enable freeswitch
