693ee214a38d52de7f254674037e209fb9d7c37f
FusionPBX <markjcrane@gmail.com>
2016-08-16 09:36:50 -0600

    Update permissions.sh

--- debian/resources/switch/permissions.sh
+++ debian/resources/switch/permissions.sh
@@ -6,3 +6,6 @@ chown -R www-data:www-data /var/run/freeswitch
 cp "$(dirname $0)/source/freeswitch.service" /lib/systemd/system/freeswitch.service
 cp "$(dirname $0)/source/etc.default.freeswitch.source" /etc/default/freeswitch
 sed -i /lib/systemd/system/freeswitch.service -e s:'-u freeswitch -g freeswitch:-u www-data -g www-data:'
+systemctl enable freeswitch
+systemctl unmask freeswitch.service
+systemctl daemon-reload
