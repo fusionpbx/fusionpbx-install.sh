8f993892f9253a271c7b31e96dddd17cb49f8282
FusionPBX <markjcrane@gmail.com>
2016-08-15 18:29:25 -0600

    Update package-permissions.sh

--- debian/resources/switch/package-permissions.sh
+++ debian/resources/switch/package-permissions.sh
@@ -1,1 +1,5 @@
-
+#setup owner and group, permissions and sticky
+chown -R www-data:www-data /etc/freeswitch
+chown -R www-data:www-data /var/lib/freeswitch
+chown -R www-data:www-data /usr/share/freeswitch
+chown -R www-data:www-data /var/log/freeswitch
