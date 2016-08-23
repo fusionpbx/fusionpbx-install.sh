a9b1110c93a400b404824a9cbdb93e02334decbb
markjcrane <markjcrane@gmail.com>
2016-08-23 00:58:46 +0000

    update source-to-package.sh

--- debian/resources/switch/source-to-package.sh
+++ debian/resources/switch/source-to-package.sh
@@ -3,3 +3,6 @@ cp -R /usr/local/freeswitch/conf /etc/freeswitch
 cp -R /usr/local/freeswitch/recordings/* /var/lib/freeswitch/recordings
 cp -R /usr/local/freeswitch/storage/* /var/lib/freeswitch/storage
 cp -R /usr/local/freeswitch/scripts/* /usr/share/freeswitch/scripts
+mkdir -p /etc/fusionpbx
+$(dirname $0)/package-release.sh
+$(dirname $0)/package-systemd.sh
