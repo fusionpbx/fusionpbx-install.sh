0e3f626c6ef43c955f8db15ca510674140ea9a9f
markjcrane <markjcrane@gmail.com>
2016-08-23 01:52:34 +0000

    Update fail2ban jail.local

--- debian/resources/switch/source-to-package.sh
+++ debian/resources/switch/source-to-package.sh
@@ -13,6 +13,10 @@ $(dirname $0)/package-release.sh
 #install freeswitch systemd.d
 $(dirname $0)/package-systemd.sh
 
+#update fail2ban
+sed -i /etc/fail2ban/jail.local -e s:'/usr/local/freeswitch/log:/var/log/freeswitch:'
+sytemctl restart fail2ban
+
 #move source files to package directories
 rsync -avz /usr/local/freeswitch/conf /etc/freeswitch
 rsync -avz /usr/local/freeswitch/recordings /var/lib/freeswitch
