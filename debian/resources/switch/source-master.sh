ab92a7422518d92b66e65b2e2588b07a008be2fc
FusionPBX <markjcrane@gmail.com>
2016-11-21 22:47:13 -0700

    Update source-master.sh

--- debian/resources/switch/source-master.sh
+++ debian/resources/switch/source-master.sh
@@ -33,7 +33,7 @@ make cd-sounds-install cd-moh-install
 
 #move the music into music/default directory
 mkdir -p /usr/local/freeswitch/sounds/music/default
-mv /usr/local/freeswitch/sounds/music/*000 /usr/share/freeswitch/sounds/music/default
+mv /usr/local/freeswitch/sounds/music/*000 /usr/local/freeswitch/sounds/music/default
 
 #configure system service
 ln -s /usr/local/freeswitch/bin/fs_cli /usr/bin/fs_cli
