f95c6a02f7cec03d4a085e9167cea136ecf0ef6e
FusionPBX <markjcrane@gmail.com>
2017-02-02 13:22:29 -0700

    Create letsencrypt.sh

---
+++ debian/resources/letsencrypt.sh
@@ -0,0 +1,9 @@
+domain_name=subdomain.domain.com
+email=username@domain.com
+
+git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+/opt/letsencrypt
+chmod 755 /opt/letsencrypt /opt/letsencrypt
+/etc/letsencrypt/./certbot-auto
+mkdir -p /etc/letsencrypt/configs
+echo "" > /etc/letsencrypt/configs/$domain_name
