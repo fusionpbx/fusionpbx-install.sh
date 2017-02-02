a6c849379329cb60f219cc6dd2f6a8f3d00d5f5a
FusionPBX <markjcrane@gmail.com>
2017-02-02 14:34:35 -0700

    Update letsencrypt.sh

--- debian/resources/letsencrypt.sh
+++ debian/resources/letsencrypt.sh
@@ -4,6 +4,8 @@ domain_name=subdomain.domain.com
 email=username@domain.com
 
 rm -R /opt/letsencrypt
+rm -R /etc/letsencrypt
+
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 chmod 755 /opt/letsencrypt/certbot-auto
 /opt/letsencrypt/./certbot-auto
@@ -14,5 +16,15 @@ mkdir -p /etc/letsencrypt/configs
 
 cp letsencrypt/domain_name.conf /etc/letsencrypt/configs/$domain_name.conf
 
-sed 's#{domain_name}#$domain_name#g' -i /etc/letsencrypt/configs/$domain_name.conf
-sed 's#{email_address}#$email#g' -i /etc/letsencrypt/configs/$domain_name.conf
+sed "s#{domain_name}#$domain_name#g" -i /etc/letsencrypt/configs/$domain_name.conf
+sed "s#{email_address}#$email#g" -i /etc/letsencrypt/configs/$domain_name.conf
+
+#letsencrypt
+sed "s@#letsencrypt@location /.well-known/acme-challenge { root /var/www/letsencrypt; }@g" -i /etc/nginx/sites-available/fusionpbx
+
+cd /opt/letsencrypt && ./letsencrypt-auto --config /etc/letsencrypt/configs/$domain_name.conf certonly
+
+sed "s@ssl_certificate         /etc/ssl/certs/nginx.crt;@ssl_certificate /etc/letsencrypt/live/$domain_name/fullchain.pem;@g" -i /etc/nginx/sites-available/fusionpbx
+sed "s@ssl_certificate_key     /etc/ssl/private/nginx.key;@ssl_certificate_key /etc/letsencrypt/live/$domain_name/privkey.pem;@g" -i /etc/nginx/sites-available/fusionpbx
+
+/usr/sbin/nginx -t && /usr/sbin/nginx -s reload
