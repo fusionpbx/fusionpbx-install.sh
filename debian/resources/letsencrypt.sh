9ddd38090c635b15183eb726f70e7d7ca93ed64a
FusionPBX <markjcrane@gmail.com>
2017-02-02 13:57:24 -0700

    Update letsencrypt.sh

--- debian/resources/letsencrypt.sh
+++ debian/resources/letsencrypt.sh
@@ -1,9 +1,18 @@
+#!/bin/sh
+
 domain_name=subdomain.domain.com
 email=username@domain.com
 
+rm -R /opt/letsencrypt
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-/opt/letsencrypt
-chmod 755 /opt/letsencrypt /opt/letsencrypt
-/etc/letsencrypt/./certbot-auto
+chmod 755 /opt/letsencrypt/certbot-auto
+/opt/letsencrypt/./certbot-auto
 mkdir -p /etc/letsencrypt/configs
-echo "" > /etc/letsencrypt/configs/$domain_name
+
+#cd $pwd
+#cd "$(dirname "$0")"
+
+cp letsencrypt/domain_name.conf /etc/letsencrypt/configs/$domain_name.conf
+
+sed 's#{domain_name}#$domain_name#g' -i /etc/letsencrypt/configs/$domain_name.conf
+sed 's#{email_address}#$email#g' -i /etc/letsencrypt/configs/$domain_name.conf
