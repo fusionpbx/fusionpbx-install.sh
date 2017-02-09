dd1cd7c1c4148f68a06d449c2dca04baec392a0c
mafoo <github@mafoo.org>
2017-02-09 14:58:14 +0000

    BugFix Relative paths
    some relative paths needed to be updated when chdir was added

--- debian/resources/nginx.sh
+++ debian/resources/nginx.sh
@@ -7,7 +7,7 @@ echo "Install the web server\n"
 apt-get install -y --force-yes nginx php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap php5-mcrypt
 
 #enable fusionpbx nginx config
-cp resources/nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
+cp nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
 ln -s /etc/nginx/sites-available/fusionpbx /etc/nginx/sites-enabled/fusionpbx
 
 #self signed certificate
