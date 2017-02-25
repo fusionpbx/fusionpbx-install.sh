74d33ea08dad1c62e0e7498f11a2ab82b972977f
erxspin <erxspin@gmail.com>
2017-02-26 00:11:13 +0600

    fixed php*-fpm service name and php.ini file path (recommited)

--- debian/resources/php.sh
+++ debian/resources/php.sh
@@ -10,12 +10,23 @@ cd "$(dirname "$0")"
 verbose "Configuring PHP"
 
 #update config if source is being used
-sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php/7.0/fpm/php.ini
-sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i /etc/php/7.0/fpm/php.ini
+if [ .$USE_PHP5_PACKAGE = .true ]; then
+        verbose "version 5.x"
+        php_ini_file='/etc/php5/fpm/php.ini'
+else
+        verbose "version 7.0"
+        php_ini_file='/etc/php/7.0/fpm/php.ini'
+fi
+sed 's#post_max_size = .*#post_max_size = 80M#g' -i $php_ini_file
+sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i $php_ini_file
 
 #restart php-fpm
 #systemd
-/bin/systemctl restart php7.0-fpm
+if [ .$USE_PHP5_PACKAGE = .true ]; then
+        systemctl restart php5-fpm
+else
+        systemctl restart php7.0-fpm
+fi
 
 #init.d
 #/usr/sbin/service php7.0-fpm restart
