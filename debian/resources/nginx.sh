5b9b55fd2c30089b1c697fd63ec8d1ed7f3c6155
erxspin <erxspin@gmail.com>
2017-02-26 00:04:53 +0600

    added choice between php5* or php7.0* packages (recommited)

--- debian/resources/nginx.sh
+++ debian/resources/nginx.sh
@@ -12,17 +12,27 @@ verbose "Installing the web server"
 arch=$(uname -m)
 real_os=$(lsb_release -is)
 codename=$(lsb_release -cs)
-if [ $real_os = 'Ubuntu' ]; then
+if [ .$USE_SWITCH_PACKAGE_UNOFFICIAL_ARM = .true ]; then
+        #9.x - */stretch/
+        #8.x - */jessie/
+        if [ .$codename = .'jessie' ]; then
+                USE_PHP5_PACKAGE = true
+        fi
+fi
+if [ .$USE_PHP5_PACKAGE = .true ]; then
+        #don't add php7.0 repository
+        verbose "Switching forcefully to php5* packages"
+elif [ .$real_os = .'Ubuntu' ]; then
         #16.10.x - */yakkety/
         #16.04.x - */xenial/
         #14.04.x - */trusty/
-        if [ $codename = 'trusty' ]; then
+        if [ .$codename = .'trusty' ]; then
                 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
         fi
 else
         #9.x - */stretch/
         #8.x - */jessie/
-        if [ $codename = 'jessie' ]; then
+        if [ .$codename = .'jessie' ]; then
                 echo "deb http://packages.dotdeb.org $codename all" > /etc/apt/sources.list.d/dotdeb.list
                 echo "deb-src http://packages.dotdeb.org $codename all" >> /etc/apt/sources.list.d/dotdeb.list
                 wget -O - https://www.dotdeb.org/dotdeb.gpg | apt-key add -
@@ -31,7 +41,12 @@ fi
 apt-get update
 
 #install dependencies
-apt-get install -y nginx php7.0 php7.0-cli php7.0-fpm php7.0-pgsql php7.0-sqlite3 php7.0-odbc php7.0-curl php7.0-imap php7.0-mcrypt php7.0-xml
+apt-get install -y nginx
+if [ .$USE_PHP5_PACKAGE = .true ]; then
+        apt-get install -y php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap php5-mcrypt
+else
+        apt-get install -y php7.0 php7.0-cli php7.0-fpm php7.0-pgsql php7.0-sqlite3 php7.0-odbc php7.0-curl php7.0-imap php7.0-mcrypt php7.0-xml
+fi
 
 #enable fusionpbx nginx config
 cp nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
