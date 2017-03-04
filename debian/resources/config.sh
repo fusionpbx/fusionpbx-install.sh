7d48203c8d95fcb60cc90ea6927e39e735621bca
FusionPBX <markjcrane@gmail.com>
2017-03-04 12:34:29 -0700

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -1,15 +1,15 @@
 
 # FusionPBX Settings
-system_branch=stable		# master, stable
+system_branch=stable            # master, stable
 
 # FreeSWITCH Settings
-switch_source=false			# true or false
-switch_package=true			# true or false
-switch_branch=stable		# master, stable
+switch_source=false             # true or false
+switch_package=true             # true or false
+switch_branch=stable            # master, stable
 
 # General Settings
-cpu_architecture=x86		# arm, x86_64
-cpu_mode=64					# 32 or 64 bit
-php_version=5				# PHP version 5 or 7
-database_version=9.6		# PostgresSQL 9.4 or 9.6
-web_server_name=nginx		# nginx
+cpu_architecture=x86            # arm, x86_64
+cpu_mode=64                     # 32 or 64 bit
+php_version=5                   # PHP version 5 or 7
+database_version=9.6            # PostgresSQL 9.4 or 9.6
+web_server_name=nginx           # nginx
