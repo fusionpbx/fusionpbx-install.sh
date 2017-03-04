75daa2acb2169cf84661ead36bb84368b3d6d308
FusionPBX <markjcrane@gmail.com>
2017-03-04 14:14:05 -0700

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -3,9 +3,10 @@
 system_branch=stable            # master, stable
 
 # FreeSWITCH Settings
+switch_branch=stable            # master, stable
 switch_source=false             # true or false
 switch_package=true             # true or false
-switch_branch=stable            # master, stable
+switch_package_all=true         # true or false
 
 # General Settings
 cpu_architecture=x86            # arm, x86_64
