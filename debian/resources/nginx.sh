4065e304009369774ca6a938059959383dd857f1
FusionPBX <markjcrane@gmail.com>
2017-03-04 21:11:08 -0700

    Update nginx.sh

--- debian/resources/nginx.sh
+++ debian/resources/nginx.sh
@@ -14,27 +14,27 @@ verbose "Installing the web server"
 arch=$(uname -m)
 real_os=$(lsb_release -is)
 codename=$(lsb_release -cs)
-if [ ."$cpu_architecture" = ."arm" ] then
+if [ ."$cpu_architecture" = ."arm" ]; then
         #9.x - */stretch/
         #8.x - */jessie/
 fi
-if [ ."$php_version" = ."5" ] then
+if [ ."$php_version" = ."5" ]; then
         #verbose "Switching forcefully to php5* packages"
-        #which add-apt-repository || apt-get install -y software-properties-common
+        which add-apt-repository || apt-get install -y software-properties-common
         #LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
         #LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php5-compat
-elif [ ."$real_os" = ."Ubuntu" ] then
+elif [ ."$real_os" = ."Ubuntu" ]; then
         #16.10.x - */yakkety/
         #16.04.x - */xenial/
         #14.04.x - */trusty/
-        if [ ."$codename" = ."trusty" ] then
+        if [ ."$codename" = ."trusty" ]; then
                 which add-apt-repository || apt-get install -y software-properties-common
                 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
         fi
 else
         #9.x - */stretch/
         #8.x - */jessie/
-        if [ ."$codename" = ."jessie" ] then
+        if [ ."$codename" = ."jessie" ]; then
                 echo "deb http://packages.dotdeb.org $codename all" > /etc/apt/sources.list.d/dotdeb.list
                 echo "deb-src http://packages.dotdeb.org $codename all" >> /etc/apt/sources.list.d/dotdeb.list
                 wget -O - https://www.dotdeb.org/dotdeb.gpg | apt-key add -
@@ -44,20 +44,20 @@ apt-get update
 
 #install dependencies
 apt-get install -y nginx
-if [ ."$php_version" = ."5" ] then
+if [ ."$php_version" = ."5" ]; then
         apt-get install -y php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap php5-mcrypt
 fi
-if [ ."$php_version" = ."7" ] then
+if [ ."$php_version" = ."7" ]; then
         apt-get install -y php7.0 php7.0-cli php7.0-fpm php7.0-pgsql php7.0-sqlite3 php7.0-odbc php7.0-curl php7.0-imap php7.0-mcrypt php7.0-xml
 fi
 
 #enable fusionpbx nginx config
 cp nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
 #prepare socket name
-if [ ."$php_version" = ."5" ] then
+if [ ."$php_version" = ."5" ]; then
         sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php5-fpm.sock;#g'
 fi
-if [ ."$php_version" = ."7" ] then
+if [ ."$php_version" = ."7" ]; then
         sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.0-fpm.sock;#g'
 fi
 ln -s /etc/nginx/sites-available/fusionpbx /etc/nginx/sites-enabled/fusionpbx
