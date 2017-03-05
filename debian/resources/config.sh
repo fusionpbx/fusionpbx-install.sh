2e93c4ca8315a5f06ffb4ecb9ffa6d652e63f8f6
FusionPBX <markjcrane@gmail.com>
2017-03-04 22:52:41 -0700

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -6,7 +6,6 @@ system_branch=stable            # master, stable
 switch_branch=stable            # master, stable
 switch_source=false             # true or false
 switch_package=true             # true or false
-switch_package_all=false         # true or false
 
 # General Settings
 php_version=5                   # PHP version 5 or 7
