a6049ad17fc6d2f23e688024c0ab984740efdee9
FusionPBX <markjcrane@gmail.com>
2017-03-04 23:06:30 -0700

    Update package-all.sh

--- debian/resources/switch/package-all.sh
+++ debian/resources/switch/package-all.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-#initialize variable encase we are called directly
-[ -z $USE_SWITCH_PACKAGE_UNOFFICIAL_ARM ] && USE_SWITCH_PACKAGE_UNOFFICIAL_ARM=false
+. ../config.sh
+. ../colors.sh
+. ../environment.sh
+. ../arguments.sh
 
 apt-get update && apt-get install -y --force-yes ntp curl memcached haveged
-arch=$(uname -m)
-if [ $arch = 'armv7l' ] && [ $USE_SWITCH_PACKAGE_UNOFFICIAL_ARM = true ]; then
+
+if [ ."$cpu_architecture" = ."arm" ]; then
         echo "deb http://repo.sip247.com/debian/freeswitch-stable-armhf/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
         curl http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
 else
