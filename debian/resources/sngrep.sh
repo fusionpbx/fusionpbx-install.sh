f4120716a465e5bb1ab5245df30b23fdc5e493f1
FusionPBX <markjcrane@gmail.com>
2017-03-05 22:27:56 -0700

    Update sngrep.sh

--- debian/resources/sngrep.sh
+++ debian/resources/sngrep.sh
@@ -2,8 +2,17 @@
 
 #add sngrep
 if [ ."$cpu_architecture" != ."arm" ]; then
+	#package
 	echo 'deb http://packages.irontec.com/debian jessie main' > /etc/apt/sources.list.d/sngrep.list
 	wget http://packages.irontec.com/public.key -q -O - | apt-key add -
 	apt-get update
 	apt-get install sngrep
+else
+	#source
+	apt-get install git autoconf automake gcc make libncurses5-dev libpcap-dev libssl-dev libpcre3-dev
+	cd /usr/src/sngrep
+	git clone https://github.com/irontec/sngrep
+	./bootstrap.sh
+	./configure
+	make install
 fi
