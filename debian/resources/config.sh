1a6d526ab0c07bf768c5d86e76720fb84de160d3
FusionPBX <markjcrane@gmail.com>
2017-03-18 22:51:37 -0600

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -1,5 +1,7 @@
 
 # FusionPBX Settings
+system_username=admin           # default username admin
+system_password=random          # random or as a pre-set value
 system_branch=stable            # master, stable
 
 # FreeSWITCH Settings
@@ -7,8 +9,10 @@ switch_branch=stable            # master, stable
 switch_source=false             # true or false
 switch_package=true             # true or false
 
-# General Settings
-php_version=5                   # PHP version 5 or 7
+# Database Settings
+database_password=random        # random or as a pre-set value
 database_repo=official          # PostgresSQL official, system, 2ndquadrant
 database_backup=false           # true or false
-web_server_name=nginx           # nginx
+
+# General Settings
+php_version=5                   # PHP version 5 or 7
