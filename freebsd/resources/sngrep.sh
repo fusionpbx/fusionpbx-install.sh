153f64f8b4aae868551cfbfb69d697c8a8f45736
Mark J Crane <markjcrane@gmail.com>
2017-03-25 15:19:57 -0600

    fix sngrep install

--- freebsd/resources/sngrep.sh
+++ freebsd/resources/sngrep.sh
@@ -6,20 +6,6 @@ cd "$(dirname "$0")"
 #includes
 . ./config.sh
 . ./colors.sh
-. ./environment.sh
 
 #add sngrep
-if [ ."$cpu_architecture" = ."arm" ]; then
-	#source install
-	apt-get install -y --force-yes git autoconf automake gcc make libncurses5-dev libpcap-dev libssl-dev libpcre3-dev
-	cd /usr/src && git clone https://github.com/irontec/sngrep
-	cd /usr/src/sngrep && ./bootstrap.sh
-	cd /usr/src/sngrep && ./configure
-	cd /usr/src/sngrep && make install
-else
-	#package install
-	echo 'deb http://packages.irontec.com/debian jessie main' > /etc/apt/sources.list.d/sngrep.list
-	wget http://packages.irontec.com/public.key -q -O - | apt-key add -
-	apt-get update
-	apt-get install sngrep
-fi
+pkg install --yes sngrep
