bc1dcdeab9c2fe73854820171a02893e7e5abb07
Mark J Crane <markjcrane@gmail.com>
2017-03-25 15:14:21 -0600

    update the switch conf and permissions

--- freebsd/resources/switch/conf-copy.sh
+++ freebsd/resources/switch/conf-copy.sh
@@ -1,3 +1,4 @@
-mv /etc/freeswitch /etc/freeswitch.orig
-mkdir /etc/freeswitch
-cp -R /var/www/fusionpbx/resources/templates/conf/* /etc/freeswitch
+mv /usr/local/freeswitch/conf /usr/local/freeswitch/conf.orig
+mkdir -p /etc/freeswitch
+mkdir -p /usr/local/freeswitch/conf
+cp -R /var/www/fusionpbx/resources/templates/conf/* /usr/local/freeswitch/conf
