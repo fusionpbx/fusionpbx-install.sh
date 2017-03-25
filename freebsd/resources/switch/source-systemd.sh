5282410a04eb6acd6b0f6effb4171b985dd99a3c
FusionPBX <markjcrane@gmail.com>
2017-03-25 15:15:07 -0600

    Delete source-systemd.sh

--- freebsd/resources/switch/source-systemd.sh
+++
@@ -1,5 +0,0 @@
-cp "$(dirname $0)/source/freeswitch.service.source" /lib/systemd/system/freeswitch.service
-cp "$(dirname $0)/source/etc.default.freeswitch" /etc/default/freeswitch
-systemctl enable freeswitch
-systemctl unmask freeswitch.service
-systemctl daemon-reload
