256d1a4f917a38a77b24ba437f8695d89b28dcbb
example <example@local.tld>
2017-03-27 18:31:33 +0600

    prepare working directory

--- pre-install.sh
+++ pre-install.sh
@@ -6,5 +6,10 @@ apt-get update && apt-get upgrade -y --force-yes
 #install git
 apt-get install -y --force-yes git
 
+#prepare dir
+if [ ! -d /usr/src ]; then
+	mkdir -vp /usr/src
+fi
+
 #get the install script
 cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
