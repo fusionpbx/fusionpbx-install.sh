616d0d3c92aac048e61cf40f2e58f35d9987f716
root <root@packer-debian-8-amd64.droplet.local>
2017-03-29 21:11:22 +0000

    update the permissions

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -2,7 +2,7 @@
 # FusionPBX Settings
 system_username=admin           # default username admin
 system_password=random          # random or as a pre-set value
-system_branch=stable            # master, stable
+system_branch=master            # master, stable
 
 # FreeSWITCH Settings
 switch_branch=stable            # master, stable
@@ -11,8 +11,8 @@ switch_package=true             # true or false
 
 # Database Settings
 database_password=random        # random or as a pre-set value
-database_repo=official          # PostgresSQL official, system, 2ndquadrant
+database_repo=2ndquadrant       # PostgresSQL official, system, 2ndquadrant
 database_backup=false           # true or false
 
 # General Settings
-php_version=5                   # PHP version 5 or 7
+php_version=7                   # PHP version 5 or 7
