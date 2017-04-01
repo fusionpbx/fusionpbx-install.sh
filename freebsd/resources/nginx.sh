eafca0e4ff658f8814f08e141d86a0fbcc19feed
FusionPBX <markjcrane@gmail.com>
2017-04-01 12:13:31 -0600

    Update nginx.sh

--- freebsd/resources/nginx.sh
+++ freebsd/resources/nginx.sh
@@ -19,12 +19,31 @@ echo 'php_fpm_enable="YES"' >> /etc/rc.conf
 
 #install dependencies
 if [ ."$php_version" = ."5" ]; then
-	pkg install --yes php56 php56-phar php56-pgsql php56-pdo php56-pdo_pgsql php56-pdo_odbc php56-pdo_sqlite php56-json php56-gd
-	pkg install --yes php56-imap php56-ldap php56-mcrypt php56-openssl php56-sockets php56-simplexml php56-xml php56-session
+	#pkg install --yes php56 php56-phar php56-pgsql php56-pdo php56-pdo_pgsql php56-pdo_odbc php56-pdo_sqlite php56-json php56-gd
+	#pkg install --yes php56-imap php56-ldap php56-mcrypt php56-openssl php56-sockets php56-simplexml php56-xml php56-session
 fi
 if [ ."$php_version" = ."7" ]; then
-	pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
-	pkg install --yes php70-imap php70-ldap php70-mcrypt php70-openssl php70-sockets php70-simplexml php70-xml php70-session
+	#pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
+	#pkg install --yes php70-imap php70-ldap php70-mcrypt php70-openssl php70-sockets php70-simplexml php70-xml php70-session
+
+	portsnap fetch update
+	cd /usr/ports/lang/php70/ && make install clean
+	cd /usr/ports/archivers/php70-phar/ && make install clean
+	cd /usr/ports/databases/php70-pgsql/ && make install clean
+	cd /usr/ports/databases/php70-pdo/ && make install clean
+	cd /usr/ports/databases/php70-pdo_pgsql/ && make install clean
+	cd /usr/ports/databases/php70-pdo_odbc/ && make install clean
+	cd /usr/ports/databases/php70-pdo_sqlite/ && make install clean
+	cd /usr/ports/devel/php70-json/ && make install clean
+	cd /usr/ports/graphics/php70-gd/ && make install clean
+	cd /usr/ports/mail/php70-imap/ && make install clean
+	cd /usr/ports/net/php70-ldap/ && make install clean
+	cd /usr/ports/security/php70-mcrypt/ && make install clean
+	cd /usr/ports/security/php70-openssl/ && make install clean
+	cd /usr/ports/net/php70-sockets/ && make install clean
+	cd /usr/ports/textproc/php70-simplexml/ && make install clean
+	cd /usr/ports/textproc/php70-xml/ && make install clean
+	cd /usr/ports/www/php70-session/ && make install clean
 fi
 
 #update the list of executables in the path
