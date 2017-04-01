291af7079757055aea2586262141bd44cdc36329
FusionPBX <markjcrane@gmail.com>
2017-04-01 15:50:03 -0600

    Update config.sh

--- freebsd/resources/config.sh
+++ freebsd/resources/config.sh
@@ -10,7 +10,7 @@ switch_source=true              # true
 
 # Database Settings
 database_password=random        # random or as a pre-set value
-database_version=9.6            # Postgres 9.6, 9.5, or 9.4
+database_version=9.3            # Postgres 9.6, 9.5, 9.4 or 9.3 (pkg php70-pgsql uses 9.3 by default)
 database_backup=false           # true or false
 
 # General Settings
