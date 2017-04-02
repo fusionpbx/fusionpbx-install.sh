add2f751b88e60477c378d5aecdf2ad723e70c66
FusionPBX <markjcrane@gmail.com>
2017-04-02 09:44:24 -0600

    Update source-release.sh

--- freebsd/resources/switch/source-release.sh
+++ freebsd/resources/switch/source-release.sh
@@ -59,6 +59,8 @@ chmod 0555 /usr/local/etc/rc.d/freeswitch
 #enable the service
 echo 'freeswitch_enable="YES"' >> /etc/rc.conf
 echo 'freeswitch_flags="-nc -nonat -reincarnate -u www -g www"' >> /etc/rc.conf
+echo 'memcached_enable="YES"' >> /etc/rc.conf
 
 #start the service
 service freeswitch start
+service memcached start
