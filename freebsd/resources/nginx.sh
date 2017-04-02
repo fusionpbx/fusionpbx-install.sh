38394f3a9abffe4d2508f817ad53d35418e3f9ff
FusionPBX <markjcrane@gmail.com>
2017-04-01 23:16:12 -0600

    Update nginx.sh

--- freebsd/resources/nginx.sh
+++ freebsd/resources/nginx.sh
@@ -30,27 +30,27 @@ if [ ."$php_version" = ."5" ]; then
 	pkg install --yes php56-imap php56-ldap php56-mcrypt php56-openssl php56-sockets php56-simplexml php56-xml php56-session
 fi
 if [ ."$php_version" = ."7" ]; then
-	pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
-	pkg install --yes php70-imap php70-ldap php70-mcrypt php70-openssl php70-sockets php70-simplexml php70-xml php70-session
+	#pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
+	#pkg install --yes php70-imap php70-ldap php70-mcrypt php70-openssl php70-sockets php70-simplexml php70-xml php70-session
 
 	#portsnap fetch update
-	#cd /usr/ports/lang/php70/ && make install clean BATCH=yes
-	#cd /usr/ports/archivers/php70-phar/ && make install clean BATCH=yes
-	#cd /usr/ports/databases/php70-pgsql/ && make install clean BATCH=yes
-	#cd /usr/ports/databases/php70-pdo/ && make install clean BATCH=yes
-	#cd /usr/ports/databases/php70-pdo_pgsql/ && make install clean BATCH=yes
-	#cd /usr/ports/databases/php70-pdo_odbc/ && make install clean BATCH=yes
-	#cd /usr/ports/databases/php70-pdo_sqlite/ && make install clean BATCH=yes
-	#cd /usr/ports/devel/php70-json/ && make install clean BATCH=yes
-	#cd /usr/ports/graphics/php70-gd/ && make install clean BATCH=yes
-	#cd /usr/ports/mail/php70-imap/ && make install clean BATCH=yes
-	#cd /usr/ports/net/php70-ldap/ && make install clean BATCH=yes
-	#cd /usr/ports/security/php70-mcrypt/ && make install clean BATCH=yes
-	#cd /usr/ports/security/php70-openssl/ && make install clean BATCH=yes
-	#cd /usr/ports/net/php70-sockets/ && make install clean BATCH=yes
-	#cd /usr/ports/textproc/php70-simplexml/ && make install clean BATCH=yes
-	#cd /usr/ports/textproc/php70-xml/ && make install clean BATCH=yes
-	#cd /usr/ports/www/php70-session/ && make install clean BATCH=yes
+	cd /usr/ports/lang/php71/ && make install clean BATCH=yes
+	cd /usr/ports/archivers/php71-phar/ && make install clean BATCH=yes
+	cd /usr/ports/databases/php71-pgsql/ && make install clean BATCH=yes
+	cd /usr/ports/databases/php71-pdo/ && make install clean BATCH=yes
+	cd /usr/ports/databases/php71-pdo_pgsql/ && make install clean BATCH=yes
+	cd /usr/ports/databases/php71-pdo_odbc/ && make install clean BATCH=yes
+	cd /usr/ports/databases/php71-pdo_sqlite/ && make install clean BATCH=yes
+	cd /usr/ports/devel/php71-json/ && make install clean BATCH=yes
+	cd /usr/ports/graphics/php71-gd/ && make install clean BATCH=yes
+	cd /usr/ports/mail/php71-imap/ && make install clean BATCH=yes
+	cd /usr/ports/net/php71-ldap/ && make install clean BATCH=yes
+	cd /usr/ports/security/php71-mcrypt/ && make install clean BATCH=yes
+	cd /usr/ports/security/php71-openssl/ && make install clean BATCH=yes
+	cd /usr/ports/net/php71-sockets/ && make install clean BATCH=yes
+	cd /usr/ports/textproc/php71-simplexml/ && make install clean BATCH=yes
+	cd /usr/ports/textproc/php71-xml/ && make install clean BATCH=yes
+	cd /usr/ports/www/php71-session/ && make install clean BATCH=yes
 fi
 
 #reset the current working directory
