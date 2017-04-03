79eb85668a6b99425d92f91ec979a4daabaade48
FusionPBX <markjcrane@gmail.com>
2017-04-02 22:23:26 -0600

    Update fail2ban.sh

--- freebsd/resources/fail2ban.sh
+++ freebsd/resources/fail2ban.sh
@@ -13,9 +13,6 @@ verbose "Installing Fail2ban"
 #add the dependencies
 pkg install --yes py27-fail2ban
 
-#update the list of executables in the path
-rehash
-
 #enable fail2ban service
 echo 'fail2ban_enable="YES"' >> /etc/rc.conf
 
@@ -28,11 +25,6 @@ cp fail2ban/fusionpbx.conf /usr/local/etc/fail2ban/filter.d/fusionpbx.conf
 cp fail2ban/nginx-404.conf /usr/local/etc/fail2ban/filter.d/nginx-404.conf
 cp fail2ban/nginx-dos.conf /usr/local/etc/fail2ban/filter.d/nginx-dos.conf
 cp fail2ban/jail.local /usr/local/etc/fail2ban/jail.local
-
-#update config if source is being used
-if [ .$switch_source = .true ]; then
-	sed -i' ' -e 's#var/log/freeswitch#usr/local/freeswitch/log#g' /usr/local/etc/fail2ban/jail.local
-fi
 
 #restart fail2ban
 service fail2ban restart
