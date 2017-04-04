39f5d2388f7ac28784940081aa9bb2308eeeb16c
FusionPBX <markjcrane@gmail.com>
2017-04-03 22:31:39 -0600

    Update pre-install.sh

--- centos/pre-install.sh
+++ centos/pre-install.sh
@@ -5,3 +5,6 @@ yum install git
 
 #get the install script
 cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
+
+#change the working directory
+cd /usr/src/fusionpbx-install.sh/centos
