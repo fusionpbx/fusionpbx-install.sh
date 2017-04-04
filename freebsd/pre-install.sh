a65e43b97f5070001a46f1449b1b8504643a2dea
FusionPBX <markjcrane@gmail.com>
2017-04-03 21:37:27 -0600

    Create pre-install.sh

---
+++ freebsd/pre-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#upgrade the packages
+apt-get update && apt-get upgrade -y --force-yes
+
+#install git
+apt-get install -y --force-yes git
+
+#get the install script
+cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
