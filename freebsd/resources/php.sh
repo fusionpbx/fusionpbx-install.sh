92f474f63b9445f35d50813806aa6c8f8ab8fd6a
FusionPBX <markjcrane@gmail.com>
2017-04-15 15:54:38 -0600

    Update php.sh

--- freebsd/resources/php.sh
+++ freebsd/resources/php.sh
@@ -11,16 +11,9 @@ cd "$(dirname "$0")"
 verbose "Configuring PHP"
 
 #update config if source is being used
-if [ ."$php_version" = ."5" ]; then
-        verbose "version 5.x"
-        php_ini_file='/etc/php5/fpm/php.ini'
-fi
-if [ ."$php_version" = ."7" ]; then
-        verbose "version 7.0"
-        php_ini_file='/etc/php/7.0/fpm/php.ini'
-fi
-sed -i' ' -e 's#post_max_size = .*#post_max_size = 80M#g' $php_ini_file
-sed -i' ' -e 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' $php_ini_file
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+sed -i' ' -e s:'post_max_size = .*:post_max_size = 80M:g' /usr/local/etc/php.ini
+sed -i' ' -e s:'upload_max_filesize = .*:upload_max_filesize = 80M:g' /usr/local/etc/php.ini
 
 #restart php-fpm
 service php-fpm restart
