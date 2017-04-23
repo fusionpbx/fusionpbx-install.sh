03f3861c43ee38aa9dc0b928c387b84e06081677
FusionPBX <markjcrane@gmail.com>
2017-04-23 02:52:42 -0600

    Create switch.sh

---
+++ centos/resources/switch.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./config.sh
+
+if [ .$switch_source = .true ]; then
+	if [ ."$switch_branch" = "master" ]; then
+		switch/source-master.sh
+	else
+		switch/source-release.sh
+	fi
+
+	#copy the switch conf files to /etc/freeswitch
+	switch/conf-copy.sh
+
+	#set the file permissions
+	switch/source-permissions.sh
+
+	#systemd service
+	switch/source-systemd.sh
+fi
+
+if [ .$switch_package = .true ]; then
+	if [ ."$switch_branch" = "master" ]; then
+		if [ .$switch_package_all = .true ]; then
+			switch/package-master-all.sh
+		else
+			switch/package-master.sh
+		fi
+	else
+		if [ .$switch_package_all = .true ]; then
+			switch/package-all.sh
+		else
+			switch/package-release.sh
+		fi
+	fi
+
+	#copy the switch conf files to /etc/freeswitch
+	switch/conf-copy.sh
+
+	#set the file permissions
+	switch/package-permissions.sh
+
+	#systemd service
+	switch/package-systemd.sh
+fi
