8b8d349b439358ea3b10a70259a9f02c7d2268ce
FusionPBX <markjcrane@gmail.com>
2017-04-24 15:36:32 -0600

    Create package-conf-copy.sh

---
+++ freebsd/resources/switch/package-conf-copy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#mv /usr/local/freeswitch/conf /usr/local/freeswitch/conf.orig
+#mkdir -p /etc/freeswitch
+#mkdir -p /usr/local/freeswitch/conf
+#cp -R /usr/local/www/fusionpbx/resources/templates/conf/* /usr/local/freeswitch/conf
