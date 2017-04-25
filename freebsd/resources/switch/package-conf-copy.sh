955d0eb807cfd8c2b7bad28b0a2e7def5d91c020
FusionPBX <markjcrane@gmail.com>
2017-04-25 02:43:44 -0600

    Update and rename package-conf-copy.sh to package-copy.sh

--- freebsd/resources/switch/package-conf-copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#mv /usr/local/etc/freeswitch /usr/local/etc/freeswitch.orig
-mkdir -p /usr/local/etc/freeswitch
-cp -R /usr/local/www/fusionpbx/resources/templates/conf/* /usr/local/etc/freeswitch
---
+++ freebsd/resources/switch/package-copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#make a backup of the conf directory
+#mv /usr/local/etc/freeswitch /usr/local/etc/freeswitch.orig
+
+#copy the default conf directory
+mkdir -p /usr/local/etc/freeswitch
+cp -R /usr/local/www/fusionpbx/resources/templates/conf/* /usr/local/etc/freeswitch
+
+#copy the scripts
+cp -R /usr/local/www/fusionpbx/resources/install/scripts /usr/local/share/freeswitch
+chown -R www:www /usr/local/share/freeswitch
