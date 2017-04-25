f775c02c5652576b24cd5e9a2299e489e2e33dbf
FusionPBX <markjcrane@gmail.com>
2017-04-25 02:46:00 -0600

    Update and rename source-conf-copy.sh to source-copy.sh

--- freebsd/resources/switch/source-conf-copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mv /usr/local/freeswitch/conf /usr/local/freeswitch/conf.orig
-#mkdir -p /etc/freeswitch
-mkdir -p /usr/local/freeswitch/conf
-cp -R /usr/local/www/fusionpbx/resources/templates/conf/* /usr/local/freeswitch/conf
