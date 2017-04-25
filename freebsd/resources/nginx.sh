6df0ae1bb4bdbdc2dc7a10e3666b480df2bfbe07
FusionPBX <markjcrane@gmail.com>
2017-04-25 00:22:50 -0600

    Update nginx.sh

--- freebsd/resources/nginx.sh
+++ freebsd/resources/nginx.sh
@@ -32,7 +32,9 @@ fi
 if [ ."$php_version" = ."7" ]; then
 	#pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
 	#pkg install --yes php70-imap php70-ldap php70-mcrypt php70-openssl php70-sockets php70-simplexml php70-xml php70-session
-	pkg install --yes postgresql96-client-9.6.2 postgresql96-contrib-9.6.2 postgresql96-server-9.6.2 php71-7.1.3 php71-phar php71-pdo php71-pdo_odbc php71-pdo_sqlite php71-json php71-gd php71-imap php71-ldap php71-mcrypt php71-openssl php71-sockets php71-simplexml php71-xml php71-session
+	pkg install --yes postgresql96-client-9.6.2 postgresql96-contrib-9.6.2 postgresql96-server-9.6.2 php71-7.1.3 
+	pkg install --yes php71-phar php71-pdo php71-pdo_odbc php71-pdo_sqlite php71-json php71-gd php71-imap php71-ldap 
+	pkg install --yes php71-mcrypt php71-openssl php71-sockets php71-simplexml php71-xml php71-session
 	cd /usr/ports/databases/php71-pdo_pgsql/ && make install clean BATCH=yes
 	cd /usr/ports/databases/php71-pgsql && make install clean BATCH=yes
 fi
