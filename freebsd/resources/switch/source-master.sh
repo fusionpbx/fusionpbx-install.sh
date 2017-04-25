e4aee1f6e0a98519894f39cbe41843fc1d03c463
FusionPBX <markjcrane@gmail.com>
2017-04-25 02:35:19 -0600

    Update source-master.sh

--- freebsd/resources/switch/source-master.sh
+++ freebsd/resources/switch/source-master.sh
@@ -58,7 +58,7 @@ chmod u-w,ugo+x /usr/local/etc/rc.d/freeswitch
 
 #enable the service
 echo 'freeswitch_enable="YES"' >> /etc/rc.conf
-echo 'freeswitch_flags="-nc -nonat -reincarnate -u www -g www"' >> /etc/rc.conf
+echo 'freeswitch_flags="-nc -nonat -u www -g www"' >> /etc/rc.conf
 
 #start the service
 service memcached start
