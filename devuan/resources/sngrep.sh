df9bdc7d52f95f6283fc2471fdcc842282205749
FusionPBX <markjcrane@gmail.com>
2017-05-14 22:26:18 -0600

    Update sngrep.sh

--- devuan/resources/sngrep.sh
+++ devuan/resources/sngrep.sh
@@ -21,5 +21,5 @@ else
 	echo 'deb http://packages.irontec.com/debian jessie main' > /etc/apt/sources.list.d/sngrep.list
 	wget http://packages.irontec.com/public.key -q -O - | apt-key add -
 	apt-get -q update
-	apt-get -y -q install sngrep
+	apt-get -y -q install -y --force-yes sngrep
 fi
