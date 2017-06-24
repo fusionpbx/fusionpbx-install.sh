e399536a5266b8553441ba43d6d2f628d0a27b7a
FusionPBX <markjcrane@gmail.com>
2017-06-23 19:42:58 -0600

    Update config.sh

--- freebsd/resources/config.sh
+++ freebsd/resources/config.sh
@@ -10,10 +10,10 @@ switch_source=source            # package, port, source
 
 # Database Settings
 database_password=random        # random or as a pre-set value
-database_version=9.6            # Postgres 9.6
+database_version=9.6            # Postgres 9.3, 9.6
 database_backup=false           # true or false
 
 # General Settings
 interface_name=auto		# auto, em0, igb0, vtnet0, or other valid names
-firewall=pf			# pf, ipfw, none
+firewall=pf			# pf, none
 php_version=7                   # PHP version 7
