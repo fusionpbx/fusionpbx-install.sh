d2e36d8cb71416d4bd38fa351c5149572da5df54
FusionPBX <markjcrane@gmail.com>
2017-07-06 10:10:29 -0600

    Update source-release.sh

--- debian/resources/switch/source-release.sh
+++ debian/resources/switch/source-release.sh
@@ -12,9 +12,6 @@ apt-get install -y --force-yes freeswitch-video-deps-most
 
 #we are about to move out of the executing directory so we need to preserve it to return after we are done
 CWD=$(pwd)
-#SWITCH_MAJOR=$(git ls-remote --heads https://freeswitch.org/stash/scm/fs/freeswitch.git "v*" | cut -d/ -f 3 | grep -P '^v\d+\.\d+' | sort | tail -n 1| cut -dv -f2)
-#SWITCH_MINOR=$(git ls-remote --tags https://freeswitch.org/stash/scm/fs/freeswitch.git v$SWITCH_MAJOR.* | cut -d/ -f3 | cut -dv -f2 | cut -d. -f3 | sort -n | tail -n1)
-#SWITCH_VERSION=$SWITCH_MAJOR.$SWITCH_MINOR
 SWITCH_VERSION=1.6.18
 echo "Using version $SWITCH_VERSION"
 cd /usr/src
