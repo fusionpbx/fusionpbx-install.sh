24928137004dd1b5a6e89f7dc73a42c99a85d0b6
FusionPBX <markjcrane@gmail.com>
2017-07-06 10:46:19 -0600

    Update source-systemd.sh

--- debian/resources/switch/source-systemd.sh
+++ debian/resources/switch/source-systemd.sh
@@ -1,5 +1,5 @@
 cp "$(dirname $0)/source/freeswitch.service.source" /lib/systemd/system/freeswitch.service
-cp "$(dirname $0)/source/etc.default.freeswitch" /etc/default/freeswitch
+cp "$(dirname $0)/source/etc.default.freeswitch.source" /etc/default/freeswitch
 systemctl enable freeswitch
 systemctl unmask freeswitch.service
 systemctl daemon-reload
