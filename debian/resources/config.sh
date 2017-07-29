b891a5c3f0382b0726e6e2e4449bc17db0aae8b0
FusionPBX <markjcrane@gmail.com>
2017-07-29 11:14:17 -0600

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -1,8 +1,8 @@
 
 # FusionPBX Settings
-domain_name=auto                # auto-ip or as a pre-set domain name or IP address
+domain_name=ip_address          # hostname, ip_address or a custom value
 system_username=admin           # default username admin
-system_password=random          # random or as a pre-set value
+system_password=random          # random or a custom value
 system_branch=stable            # master, stable
 
 # FreeSWITCH Settings
@@ -11,8 +11,8 @@ switch_source=false             # true or false
 switch_package=true             # true or false
 
 # Database Settings
-database_password=random        # random or as a pre-set value
-database_repo=official       # PostgresSQL official, system, 2ndquadrant
+database_password=random        # random or a custom value
+database_repo=official          # PostgresSQL official, system, 2ndquadrant
 database_backup=false           # true or false
 
 # General Settings
