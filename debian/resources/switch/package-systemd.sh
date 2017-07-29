0042437ad06f719dc6356fa1a6e08972f5620856
danry25 <adn5412@gmail.com>
2017-07-29 12:49:51 -0700

    Added minimal OpenVZ support. (#107)

--- debian/resources/switch/package-systemd.sh
+++ debian/resources/switch/package-systemd.sh
@@ -2,6 +2,11 @@ apt-get remove -y --force-yes freeswitch-systemd
 cp "$(dirname $0)/source/freeswitch.service.package" /lib/systemd/system/freeswitch.service
 cp "$(dirname $0)/source/etc.default.freeswitch.package" /etc/default/freeswitch
 chmod 644 /lib/systemd/system/freeswitch.service 
+if [ -e /proc/user_beancounters ]
+then
+    echo "Oh, your on OpenVZ! Setting a CPU Scheduler isn't possible :("
+    sed -i -e "s/CPUSchedulingPolicy=rr/;CPUSchedulingPolicy=rr/g" /lib/systemd/system/freeswitch.service
+fi
 systemctl enable freeswitch
 systemctl unmask freeswitch.service
 systemctl daemon-reload
