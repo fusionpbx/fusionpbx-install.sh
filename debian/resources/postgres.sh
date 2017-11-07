c5c55bb35abcd01efc3b1aa32e31cdbd9f912ec7
FusionPBX <markjcrane@gmail.com>
2017-11-06 20:54:06 -0700

    Update postgres.sh

--- debian/resources/postgres.sh
+++ debian/resources/postgres.sh
@@ -32,7 +32,6 @@ if [ ."$database_repo" = ."official" ]; then
 	echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' > /etc/apt/sources.list.d/postgresql.list
 	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 	apt-get update && apt-get upgrade -y
-	apt-get install -y --force-yes sudo postgresql
 	if [ ."$database_version" = ."latest" ]; then
                 apt-get install -y --force-yes sudo postgresql
 	fi
