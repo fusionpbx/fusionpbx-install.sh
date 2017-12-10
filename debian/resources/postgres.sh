55b9b4fbc9c88fd0248325c0c1f27465733606dd
Len <Len-PGH@users.noreply.github.com>
2017-12-09 21:23:06 -0500

    Update postgres.sh

--- debian/resources/postgres.sh
+++ debian/resources/postgres.sh
@@ -17,11 +17,6 @@ password=$(dd if=/dev/urandom bs=1 count=20 2>/dev/null | base64)
 #install message
 echo "Install PostgreSQL and create the database and users\n"
 
-#use the sip247 database repo for arm
-if [ .$cpu_architecture = .'arm' ]; then
-        database_repo="sip247"
-fi
-
 #included in the distribution
 if [ ."$database_repo" = ."system" ]; then
 	apt-get install -y --force-yes sudo postgresql
@@ -50,13 +45,6 @@ if [ ."$database_repo" = ."2ndquadrant" ]; then
 	apt-get install -y --force-yes sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4
 fi
 
-#sip247 arm repository
-if [ ."$database_repo" = ."sip247" ]; then
-        echo 'deb http://repo.sip247.com/debian/postgresql-armhf jessie main' > /etc/apt/sources.list.d/pgsql-sip247.list
-        wget --quiet -O - http://repo.sip247.com/debian/sip247.com.gpg.key | apt-key add -
-        apt-get update && apt-get upgrade -y
-        apt-get install -y --force-yes sudo postgresql
-fi
 
 #systemd
 systemctl daemon-reload
