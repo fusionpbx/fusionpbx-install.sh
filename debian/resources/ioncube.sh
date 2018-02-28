58892a8f7c6eaf16563c93249a53b017de4869d9
FusionPBX <markjcrane@gmail.com>
2018-02-28 01:20:33 -0700

    Create ioncube.sh

---
+++ debian/resources/ioncube.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./config.sh
+. ./colors.sh
+. ./environment.sh
+
+#make sure unzip is install
+apt-get install unzip
+
+#get the ioncube 64 bit loader
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip
+
+#uncompress the file
+unzip ioncube_loaders_lin_x86-64.zip
+
+#remove the zip file
+rm ioncube_loaders_lin_x86-64.zip
+
+#copy the loader to the correct directory
+if [ ."$php_version" = ."5" ]; then
+        #copy the php extension .so into the php lib directory
+        cp ioncube/ioncube_loader_lin_5.6.so /usr/lib/php/20151012
+fi
+if [ ."$php_version" = ."7" ]; then
+        #copy the php extension .so into the php lib directory
+        cp ioncube/ioncube_loader_lin_7.0.so /usr/lib/php/20151012
+
+        #add the 00-ioncube.ini file
+        echo "zend_extension = /usr/lib/php/20151012/ioncube_loader_lin_7.0.so" > /etc/php/7.0/fpm/conf.d/00-ioncube.ini
+
+        #restart php fpm
+        service php7.0-fpm restart
+fi
