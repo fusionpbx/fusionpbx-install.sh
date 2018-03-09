ffb38c912c7c60598efc85eda779bf941c6d16a5
Len <Len-PGH@users.noreply.github.com>
2018-03-08 21:54:31 -0500

    Update nginx.sh
    updated for stretch

--- debian/resources/nginx.sh
+++ debian/resources/nginx.sh
@@ -10,7 +10,9 @@ cd "$(dirname "$0")"
 
 #change the version of php for debian stretch
 if [ ."$os_codename" = ."stretch" ]; then
-       php_version=7
+        apt-get -y install apt-transport-https lsb-release ca-certificates
+        wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+        sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 fi
 
 #send a message
