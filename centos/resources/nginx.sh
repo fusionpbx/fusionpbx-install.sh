b6e0d9e5418cc40bccb0db6560e8ac3b8592ba60
FusionPBX <markjcrane@gmail.com>
2018-05-02 12:47:47 -0600

    Update nginx.sh

--- centos/resources/nginx.sh
+++ centos/resources/nginx.sh
@@ -23,5 +23,8 @@ ln -s /etc/nginx/sites-available/fusionpbx.conf /etc/nginx/sites-enabled/fusionp
 awk '/server *{/ {c=1 ; next} c && /{/{c++} c && /}/{c--;next} !c' /etc/nginx/nginx.conf > /etc/nginx/nginx.tmp && mv -f /etc/nginx/nginx.tmp /etc/nginx/nginx.conf && rm -f /etc/nginx/nginx.tmp
 sed -i '/include \/etc\/nginx\/conf\.d\/\*\.conf\;/a \    include \/etc\/nginx\/sites-enabled\/\*\.conf\;' /etc/nginx/nginx.conf
 
+#set the log permissions
+chmod -R 664 /var/log/nginx/
+
 #send a message
 verbose "nginx installed"
