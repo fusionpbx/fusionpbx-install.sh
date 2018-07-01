c5e04d948da98cf35b56f1f17af66849a253e0cc
FusionPBX <markjcrane@gmail.com>
2018-07-01 09:34:40 -0600

    Update letsencrypt.sh

--- freebsd/resources/letsencrypt.sh
+++ freebsd/resources/letsencrypt.sh
@@ -4,7 +4,7 @@
 # Mark J Crane <markjcrane@fusionpbx.com>
 # Copyright (C) 2018
 # All Rights Reserved.
-      
+
 #move to script directory so all relative paths work
 cd "$(dirname "$0")"
 
@@ -12,11 +12,11 @@ cd "$(dirname "$0")"
 . ./config.sh
 
 #remove dehyrdated letsencrypt script
-#rm -R /etc/dehydrated/
+#rm -R /usr/local/etc/dehydrated/
 #rm /usr/local/sbin/dehydrated
 #rm -R /usr/src/dehydrated
 #rm -R /usr/src/dns-01-manual
-#rm -R /var/www/dehydrated
+#rm -R /usr/local/www/dehydrated
 
 #request the domain name, email address and wild card domain
 read -p 'Domain Name: ' domain_name
@@ -34,8 +34,8 @@ fi
 cd /usr/src && git clone https://github.com/lukas2511/dehydrated.git
 cd /usr/src/dehydrated
 cp dehydrated /usr/local/sbin
-mkdir -p /var/www/dehydrated
-mkdir -p /etc/dehydrated/certs
+mkdir -p /usr/local/www/dehydrated
+mkdir -p /usr/local/etc/dehydrated/certs
 
 #remove the wildcard and period
 if [ .$wilcard_domain = ."y" ]; then
@@ -44,46 +44,46 @@ fi
 
 #create an alias when using wildcard dns
 #if [ .$wilcard_domain = ."y" ]; then
-#  echo "*.$domain_name > $domain_name" > /etc/dehydrated/domains.txt
+#  echo "*.$domain_name > $domain_name" > /usr/local/etc/dehydrated/domains.txt
 #fi
 
 #manual dns hook
 cd /usr/src
 git clone https://github.com/owhen/dns-01-manual.git
 cd /usr/src/dns-01-manual/
-cp hook.sh /etc/dehydrated/hook.sh
-chmod 755 /etc/dehydrated/hook.sh
+cp hook.sh /usr/local/etc/dehydrated/hook.sh
+chmod 755 /usr/local/etc/dehydrated/hook.sh
 
-#copy config and hook.sh into /etc/dehydrated
+#copy config and hook.sh into /usr/local/etc/dehydrated
 cd /usr/src/dehydrated
-cp docs/examples/config /etc/dehydrated
-#cp docs/examples/hook.sh /etc/dehydrated
+cp docs/examples/config /usr/local/etc/dehydrated
+#cp docs/examples/hook.sh /usr/local/etc/dehydrated
 
-#vim /etc/dehydrated/config
-#sed "s#CONTACT_EMAIL=#CONTACT_EMAIL=$email_address" -i /etc/dehydrated/config
+#vim /usr/local/etc/dehydrated/config
+#sed "s#CONTACT_EMAIL=#CONTACT_EMAIL=$email_address" -i /usr/local/etc/dehydrated/config
 
 #make sure the nginx ssl directory exists
 mkdir -p /usr/local/etc/nginx/ssl
 
 #accept the terms
-dehydrated --register --accept-terms --config /etc/dehydrated/config
+dehydrated --register --accept-terms --config /usr/local/etc/dehydrated/config
 
 #wildcard domain
 if [ .$wilcard_domain = ."y" ]; then
-  dehydrated --cron --domain *.$domain_name --alias $domain_name --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge dns-01 --hook /etc/dehydrated/hook.sh
+  dehydrated --cron --domain *.$domain_name --alias $domain_name --config /usr/local/etc/dehydrated/config --out /usr/local/etc/dehydrated/certs --challenge dns-01 --hook /usr/local/etc/dehydrated/hook.sh
 fi
 
 #single domain
 if [ .$wilcard_domain = ."n" ]; then
-  dehydrated --cron --domain $domain_name --config /etc/dehydrated/config --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge dns-01 --hook /etc/dehydrated/hook.sh
+  dehydrated --cron --domain $domain_name --config /usr/local/etc/dehydrated/config --config /usr/local/etc/dehydrated/config --out /usr/local/etc/dehydrated/certs --challenge dns-01 --hook /usr/local/etc/dehydrated/hook.sh
 fi
 
 #update nginx config
-sed "s@ssl_certificate         /etc/ssl/certs/nginx.crt;@ssl_certificate /etc/dehydrated/certs/$domain_name/fullchain.pem;@g" -i /usr/local/etc/nginx/sites-available/fusionpbx
-sed "s@ssl_certificate_key     /etc/ssl/private/nginx.key;@ssl_certificate_key /etc/dehydrated/certs/$domain_name/privkey.pem;@g" -i /usr/local/etc/nginx/sites-available/fusionpbx
+sed "s@ssl_certificate         /etc/ssl/certs/nginx.crt;@ssl_certificate /usr/local/etc/dehydrated/certs/$domain_name/fullchain.pem;@g" -i /usr/local/etc/nginx/sites-available/fusionpbx
+sed "s@ssl_certificate_key     /etc/ssl/private/nginx.key;@ssl_certificate_key /usr/local/etc/dehydrated/certs/$domain_name/privkey.pem;@g" -i /usr/local/etc/nginx/sites-available/fusionpbx
 
 #read the config
-/usr/sbin/nginx -t && /usr/sbin/nginx -s reload
+/usr/local/sbin/nginx -t && /usr/local/sbin/nginx -s reload
 
 #make sure the freeswitch directory exists
 mkdir -p /usr/local/etc/freeswitch/tls
@@ -92,15 +92,15 @@ mkdir -p /usr/local/etc/freeswitch/tls
 rm /usr/local/etc/freeswitch/tls/*
 
 #combine the certs into all.pem
-cat /etc/dehydrated/certs/$domain_name/fullchain.pem > /usr/local/etc/freeswitch/tls/all.pem
-cat /etc/dehydrated/certs/$domain_name/privkey.pem >> /usr/local/etc/freeswitch/tls/all.pem
-#cat /etc/dehydrated/certs/$domain_name/chain.pem >> /usr/local/etc/freeswitch/tls/all.pem
+cat /usr/local/etc/dehydrated/certs/$domain_name/fullchain.pem > /usr/local/etc/freeswitch/tls/all.pem
+cat /usr/local/etc/dehydrated/certs/$domain_name/privkey.pem >> /usr/local/etc/freeswitch/tls/all.pem
+#cat /usr/local/etc/dehydrated/certs/$domain_name/chain.pem >> /usr/local/etc/freeswitch/tls/all.pem
 
 #copy the certificates
-cp /etc/dehydrated/certs/$domain_name/cert.pem /usr/local/etc/freeswitch/tls
-cp /etc/dehydrated/certs/$domain_name/chain.pem /usr/local/etc/freeswitch/tls
-cp /etc/dehydrated/certs/$domain_name/fullchain.pem /usr/local/etc/freeswitch/tls
-cp /etc/dehydrated/certs/$domain_name/privkey.pem /usr/local/etc/freeswitch/tls
+cp /usr/local/etc/dehydrated/certs/$domain_name/cert.pem /usr/local/etc/freeswitch/tls
+cp /usr/local/etc/dehydrated/certs/$domain_name/chain.pem /usr/local/etc/freeswitch/tls
+cp /usr/local/etc/dehydrated/certs/$domain_name/fullchain.pem /usr/local/etc/freeswitch/tls
+cp /usr/local/etc/dehydrated/certs/$domain_name/privkey.pem /usr/local/etc/freeswitch/tls
 
 #add symbolic links
 ln -s /usr/local/etc/freeswitch/tls/all.pem /usr/local/etc/freeswitch/tls/agent.pem
