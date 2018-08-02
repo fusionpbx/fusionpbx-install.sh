8bca6b7de391c063ebdbdf7f47f51969ae32d63e
FusionPBX <markjcrane@gmail.com>
2018-08-02 12:50:35 -0600

    Update letsencrypt.sh

--- debian/resources/letsencrypt.sh
+++ debian/resources/letsencrypt.sh
@@ -79,10 +79,10 @@ fi
 
 #request the certificates
 if [ .$wilcard_domain = ."true" ]; then
-  dehydrated --cron --domain *.$domain_name --alias $domain_alias --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge dns-01 --hook /etc/dehydrated/hook.sh
+	dehydrated --cron --domain *.$domain_name --alias $domain_alias --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge dns-01 --hook /etc/dehydrated/hook.sh
 fi
 if [ .$wilcard_domain = ."false" ]; then
-  dehydrated --cron --alias $domain_alias --config /etc/dehydrated/config --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge http-01
+	dehydrated --cron --alias $domain_alias --config /etc/dehydrated/config --config /etc/dehydrated/config --out /etc/dehydrated/certs --challenge http-01
 fi
 
 #make sure the nginx ssl directory exists
