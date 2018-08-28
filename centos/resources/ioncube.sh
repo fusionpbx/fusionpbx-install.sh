c8bb065108714bbaf22ce523edbf33c962f3dc81
FusionPBX <markjcrane@gmail.com>
2018-08-28 11:48:54 -0600

    Create ioncube.sh

---
+++ centos/resources/ioncube.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./config.sh
+. ./colors.sh
+. ./environment.sh
+
+#make sure unzip is install
+yum install unzip
+
+#get the ioncube 64 bit loader
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip
+
+#uncompress the file
+unzip ioncube_loaders_lin_x86-64.zip
+
+#remove the zip file
+rm ioncube_loaders_lin_x86-64.zip
+
+#copy the php extension .so into the php lib directory
+cp ioncube/ioncube_loader_lin_7.1.so /usr/lib64/php/modules
+
+#add the 00-ioncube.ini file
+echo "zend_extension = /usr/lib64/php/modules/ioncube_loader_lin_7.1.so" > /etc/php.d/00-ioncube.ini
+
+#restart the service
+service php7.1-fpm restart
+
