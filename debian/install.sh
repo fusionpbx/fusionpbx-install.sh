0ac1c952aefdcfd115c979899f0a9452dea5a7a4
FusionPBX <markjcrane@gmail.com>
2018-10-10 18:49:01 -0600

    Update install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -27,6 +27,7 @@ apt-get install -y nano
 #SNMP
 apt-get install -y snmpd
 echo "rocommunity public" > /etc/snmp/snmpd.conf
+service snmpd restart
 
 #IPTables
 resources/iptables.sh
@@ -57,14 +58,21 @@ server_address=$(hostname -I)
 
 #restart services
 systemctl daemon-reload
-if [ ."$php_version" = ."5" ]; then
-        systemctl restart php5-fpm
+if [ ."$php_version" = ."5.6" ]; then
+        service php5-fpm restart
 fi
-if [ ."$php_version" = ."7" ]; then
-        systemctl restart php7.1-fpm
+if [ ."$php_version" = ."7.0" ]; then
+        service php7.0-fpm restart
 fi
-systemctl restart nginx
-systemctl restart fail2ban
+if [ ."$php_version" = ."7.1" ]; then
+        service php7.1-fpm restart
+fi
+if [ ."$php_version" = ."7.2" ]; then
+        service php7.2-fpm restart
+fi
+
+service nginx restart
+service fail2ban restart
 
 #add the database schema, user and groups
 resources/finish.sh
