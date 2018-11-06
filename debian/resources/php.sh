e052971f522ebacc6e211ec2408a40a665ab3796
FusionPBX <markjcrane@gmail.com>
2018-11-05 23:13:08 -0700

    Update php.sh

--- debian/resources/php.sh
+++ debian/resources/php.sh
@@ -12,7 +12,11 @@ verbose "Configuring PHP"
 
 #change the version of php for debian stretch
 if [ ."$os_codename" = ."stretch" ]; then
-       php_version=7.0
+       if [ ."$cpu_architecture" = ."arm" ]; then
+              php_version=7.2
+       else
+              php_version=7.0
+       fi 
 fi
 
 #update config if source is being used
