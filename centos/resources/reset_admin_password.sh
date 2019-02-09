a636d1a410d724d8e6274bbbe58f5190043bf9fe
FusionPBX <markjcrane@gmail.com>
2019-02-09 11:05:47 -0700

    Update reset_admin_password.sh

--- centos/resources/reset_admin_password.sh
+++ centos/resources/reset_admin_password.sh
@@ -9,14 +9,14 @@ cd "$(dirname "$0")"
 . ./environment.sh
 
 #count the users
-admin_users=$(sudo -u postgres psql fusionpbx -Atc "select count(*) from v_users JOIN v_group_users USING (domain_uuid) where username='$system_username' and group_name = 'superadmin'")
+admin_users=$(sudo -u postgres psql fusionpbx -Atc "select count(*) from v_users JOIN v_user_groups USING (domain_uuid) where username='$system_username' and group_name = 'superadmin'")
 
 if [ .$admin_users = .'0' ]; then
 	error "i could not find the user '$system_username' in the database, check your resources/config.sh is correct"
 elif [ .$admin_users = .'' ]; then
 	error "something went wrong, see errors above";
 else
-	admin_uuids=$(sudo -u postgres psql fusionpbx -Atc "select v_users.user_uuid from v_users JOIN v_group_users USING (domain_uuid) where username='$system_username' and group_name = 'superadmin'")
+	admin_uuids=$(sudo -u postgres psql fusionpbx -Atc "select v_users.user_uuid from v_users JOIN v_user_groups USING (domain_uuid) where username='$system_username' and group_name = 'superadmin'")
 	for admin_uuid in $admin_uuids; do
 		user_salt=$(/usr/bin/php /var/www/fusionpbx/resources/uuid.php);
 		if [ .$system_password = .'random' ]; then
