03a4cb91a77190f0d8b5cb563407a96f96d37c57
FusionPBX <markjcrane@gmail.com>
2019-02-17 09:31:57 -0700

    Update fusionpbx.sh

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -11,8 +11,8 @@ cd "$(dirname "$0")"
 verbose "Installing FusionPBX"
 
 #install dependencies
-apt-get install -y --force-yes vim git dbus haveged ssl-cert qrencode
-apt-get install -y --force-yes ghostscript libtiff5-dev libtiff-tools at
+apt-get install -y vim git dbus haveged ssl-cert qrencode
+apt-get install -y ghostscript libtiff5-dev libtiff-tools at
 
 #get the branch
 if [ .$system_branch = .'master' ]; then
