348c82e7001646fa63d99588ac6a552476e5d8e8
FusionPBX <markjcrane@gmail.com>
2019-02-17 09:44:51 -0700

    Update pre-install.sh

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #upgrade the packages
-apt-get update && apt-get upgrade -y --force-yes
+apt-get update && apt-get upgrade -y
 
 #install packages
-apt-get install -y --force-yes git lsb-release
+apt-get install -y git lsb-release
 
 #get the install script
 cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
