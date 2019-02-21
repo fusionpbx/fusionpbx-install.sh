c6cc22912027801565991baa5db1aee6a56c3e8a
FusionPBX <markjcrane@gmail.com>
2019-02-21 10:04:23 -0700

    Update source-release.sh

--- debian/resources/switch/source-release.sh
+++ debian/resources/switch/source-release.sh
@@ -1,40 +1,55 @@
 #!/bin/sh
 
-echo "Installing the FreeSWITCH source"
-DEBIAN_FRONTEND=none APT_LISTCHANGES_FRONTEND=none apt-get install -y ntpdate libapache2-mod-log-sql-ssl libfreetype6-dev git-buildpackage doxygen yasm nasm gdb git build-essential automake autoconf 'libtool-bin|libtool' python uuid-dev zlib1g-dev 'libjpeg8-dev|libjpeg62-turbo-dev' libncurses5-dev libssl-dev libpcre3-dev libcurl4-openssl-dev libldns-dev libedit-dev libspeexdsp-dev libspeexdsp-dev libsqlite3-dev perl libgdbm-dev libdb-dev bison libvlc-dev libvlccore-dev vlc-nox pkg-config ccache libpng-dev libvpx-dev libyuv-dev libopenal-dev libbroadvoice-dev libcodec2-dev libflite-dev libg7221-dev libilbc-dev libmongoc-dev libsilk-dev libsoundtouch-dev libmagickcore-dev liblua5.2-dev libopus-dev libsndfile-dev libopencv-dev libavformat-dev libx264-dev erlang-dev libldap2-dev libmemcached-dev libperl-dev portaudio19-dev python-dev libsnmp-dev libyaml-dev libmp4v2-dev
-apt-get install -y ntp unzip libpq-dev memcached libshout3-dev libvpx-dev libmpg123-dev libmp3lame-dev
+# add repo key
+wget -O - https://files.freeswitch.org/repo/deb/freeswitch-1.8/fsstretch-archive-keyring.asc | apt-key add -
 
-apt-get update && apt-get install -y curl haveged
-curl https://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add -
-echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
-apt-get update && apt-get upgrade
-apt-get install -y freeswitch-video-deps-most
+# add repo
+echo "deb  http://files.freeswitch.org/repo/deb/freeswitch-1.8/  stretch main" > /etc/apt/sources.list.d/freeswitch.list
+echo "deb-src  http://files.freeswitch.org/repo/deb/freeswitch-1.8/  stretch main" >> /etc/apt/sources.list.d/freeswitch.list
+
+#upgrade packages
+apt update && apt upgrade
+
+# install dependencies
+apt-get build-dep freeswitch
+
+# install dependencies
+apt-get build-dep freeswitch
+
+# additional dependencies
+apt install sqlite3 swig3.0 unzip
 
 #we are about to move out of the executing directory so we need to preserve it to return after we are done
 CWD=$(pwd)
-SWITCH_VERSION=1.6.20
+SWITCH_VERSION=1.8.2
 echo "Using version $SWITCH_VERSION"
 cd /usr/src
-#git clone -b v1.6 https://freeswitch.org/stash/scm/fs/freeswitch.git /usr/src/freeswitch
+#git clone -b v1.8 https://freeswitch.org/stash/scm/fs/freeswitch.git /usr/src/freeswitch
 wget http://files.freeswitch.org/freeswitch-releases/freeswitch-$SWITCH_VERSION.zip
 unzip freeswitch-$SWITCH_VERSION.zip
 rm -R freeswitch
 mv freeswitch-$SWITCH_VERSION freeswitch
-cd freeswitch
+cd /usr/src/freeswitch
 
+# bootstrap is needed if using git
 #./bootstrap.sh -j
+
+# enable required modules
 sed -i /usr/src/freeswitch/modules.conf -e s:'#applications/mod_avmd:applications/mod_avmd:'
 sed -i /usr/src/freeswitch/modules.conf -e s:'#applications/mod_callcenter:applications/mod_callcenter:'
 sed -i /usr/src/freeswitch/modules.conf -e s:'#applications/mod_cidlookup:applications/mod_cidlookup:'
 sed -i /usr/src/freeswitch/modules.conf -e s:'#applications/mod_memcache:applications/mod_memcache:'
 sed -i /usr/src/freeswitch/modules.conf -e s:'#applications/mod_curl:applications/mod_curl:'
 sed -i /usr/src/freeswitch/modules.conf -e s:'#formats/mod_shout:formats/mod_shout:'
-#./configure --help
-#./configure --prefix=/usr/local/freeswitch --enable-core-pgsql-support --enable-system-lua --disable-fhs
-./configure --prefix=/usr/local/freeswitch --enable-core-pgsql-support --disable-fhs
-#make mod_shout-install
+
+# prepare the build
+#./configure --prefix=/usr/local/freeswitch --enable-core-pgsql-support --disable-fhs
+./configure -C --enable-portable-binary --disable-dependency-tracking \
+--prefix=/usr --localstatedir=/var --sysconfdir=/etc \
+--with-openssl --enable-core-pgsql-support
+
+# compile and install
 make
-rm -rf /usr/local/freeswitch/{lib,mod,bin}/*
 make install
 make sounds-install moh-install
 make hd-sounds-install hd-moh-install
@@ -46,6 +61,3 @@ mv /usr/local/freeswitch/sounds/music/*000 /usr/local/freeswitch/sounds/music/de
 
 #return to the executing directory
 cd $CWD
-
-#symbolic link for fs_cli
-ln -s /usr/local/freeswitch/bin/fs_cli /usr/bin/fs_cli
