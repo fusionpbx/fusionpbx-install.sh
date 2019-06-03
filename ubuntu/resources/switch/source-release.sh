12665ba192fb371ceb8a9563a4de563ec763e2b5
FusionPBX <markjcrane@gmail.com>
2019-06-03 16:42:27 -0600

    Update source-release.sh

--- ubuntu/resources/switch/source-release.sh
+++ ubuntu/resources/switch/source-release.sh
@@ -12,9 +12,9 @@ apt update && apt upgrade -y
 # install dependencies
 apt install -y autoconf automake devscripts g++ git-core libncurses5-dev libtool make libjpeg-dev 
 apt install -y pkg-config flac  libgdbm-dev libdb-dev gettext sudo equivs mlocate git dpkg-dev 
-apt intsall -y libtiff5-dev libperl-dev libcurl4-openssl-dev libuuid libsqlite3-dev libpcre3-dev
+apt install -y liblua5.2-dev libtiff5-dev libperl-dev libcurl4-openssl-dev libsqlite3-dev libpcre3-dev
 apt install -y devscripts libspeexdsp-dev libspeex-dev libldns-dev libedit-dev libopus-dev libmemcached-dev
-apt install -y libshout3-dev libmpg123-dev libmp3lame-dev yasm nasm libsndfile1-dev liblua5.2-dev libuv1-dev libvpx-dev libvpx5
+apt install -y libshout3-dev libmpg123-dev libmp3lame-dev yasm nasm libsndfile1-dev libuv1-dev libvpx-dev libvpx5
 
 # additional dependencies
 apt install -y swig3.0 unzip sox wget
