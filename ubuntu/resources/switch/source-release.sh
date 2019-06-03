507fa386c0d7e54fc40231aff8e7b2ffa0cc05d6
FusionPBX <markjcrane@gmail.com>
2019-06-03 08:49:13 -0600

    Update source-release.sh

--- ubuntu/resources/switch/source-release.sh
+++ ubuntu/resources/switch/source-release.sh
@@ -50,9 +50,9 @@ sed -i /usr/src/freeswitch/modules.conf -e s:'#formats/mod_shout:formats/mod_sho
 # compile and install
 make
 make install
-make sounds-install moh-install
-make hd-sounds-install hd-moh-install
-make cd-sounds-install cd-moh-install
+#make sounds-install moh-install
+#make hd-sounds-install hd-moh-install
+#make cd-sounds-install cd-moh-install
 
 #move the music into music/default directory
 mkdir -p /usr/share/freeswitch/sounds/music/default
