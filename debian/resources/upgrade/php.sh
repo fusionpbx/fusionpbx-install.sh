1eee6a065b13cb9f88273702cf650cf56b4f5744
FusionPBX <markjcrane@gmail.com>
2019-07-25 16:54:57 -0600

    Update php.sh

--- debian/resources/upgrade/php.sh
+++ debian/resources/upgrade/php.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+php_version=7.1
+
 #remove php5
 apt remove -y php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap php5-mcrypt php5-gd
 
@@ -15,11 +17,11 @@ wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 apt-get update
 
-#install php 7.1
-apt-get install -y php7.1 php7.1-cli php7.1-fpm php7.1-pgsql php7.1-sqlite3 php7.1-odbc php7.1-curl php7.1-imap php7.1-mcrypt php7.1-xml php7.1-gd
+#install php
+apt-get install -y php${php_version} php${php_version}-cli php${php_version}-fpm php${php_version}-pgsql php${php_version}-sqlite3 php${php_version}-odbc php${php_version}-curl php${php_version}-imap php${php_version}-mcrypt php${php_version}-xml php${php_version}-gd php${php_version}-ldap
 
 #update the unix socket name
-sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.1-fpm.sock;#g'
+sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php${php_version}-fpm.sock;#g'
 
 #restart nginx
 service nginx restart
