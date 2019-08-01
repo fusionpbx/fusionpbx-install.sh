a1e94a036ac4165a7905b324eb354a1654c34a8d
FusionPBX <markjcrane@gmail.com>
2019-08-01 13:04:20 -0600

    Update php.sh

--- devuan/resources/php.sh
+++ devuan/resources/php.sh
@@ -21,6 +21,7 @@ if [ ."$php_version" = ."7" ]; then
 fi
 sed 's#post_max_size = .*#post_max_size = 80M#g' -i $php_ini_file
 sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i $php_ini_file
+sed 's#; max_input_vars = .*#max_input_vars = 8000#g' -i $php_ini_file
 
 #restart php-fpm
 if [ ."$php_version" = ."5" ]; then
