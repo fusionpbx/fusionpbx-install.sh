ff147ea6674e90b874f5603c25928e7bb321d4cf
FusionPBX <markjcrane@gmail.com>
2019-08-01 12:59:58 -0600

    Update php.sh

--- freebsd/resources/php.sh
+++ freebsd/resources/php.sh
@@ -75,6 +75,7 @@ verbose "Configuring PHP"
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 sed -i' ' -e s:'post_max_size = .*:post_max_size = 80M:g' /usr/local/etc/php.ini
 sed -i' ' -e s:'upload_max_filesize = .*:upload_max_filesize = 80M:g' /usr/local/etc/php.ini
+sed -i' ' -e s:'; max_input_vars = .*:max_input_vars = 8000:g' /usr/local/etc/php.ini
 
 #restart php-fpm
 service php-fpm restart
