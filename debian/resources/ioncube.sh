25ec4ae43551bf5e2617f6d728845cbf0bc437f4
FusionPBX <markjcrane@gmail.com>
2019-08-20 17:09:52 -0600

    Update ioncube.sh

--- debian/resources/ioncube.sh
+++ debian/resources/ioncube.sh
@@ -70,3 +70,13 @@ if [ ."$php_version" = ."7.1" ]; then
         #restart the service
         service php7.1-fpm restart
 fi
+if [ ."$php_version" = ."7.3" ]; then
+        #copy the php extension .so into the php lib directory
+        cp ioncube/ioncube_loader_lin_7.3.so /usr/lib/php/20180731
+
+        #add the 00-ioncube.ini file
+        echo "zend_extension = /usr/lib/php/20160303/ioncube_loader_lin_7.3.so" > /etc/php/7.3/fpm/conf.d/00-ioncube.ini
+
+        #restart the service
+        service php7.3-fpm restart
+fi
