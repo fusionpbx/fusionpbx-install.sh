01ece286903c866a774ba73de5c47fe486fa7262
FusionPBX <markjcrane@gmail.com>
2019-08-22 08:17:58 -0600

    Update package-release.sh

--- debian/resources/switch/package-release.sh
+++ debian/resources/switch/package-release.sh
@@ -10,9 +10,17 @@ cd "$(dirname "$0")"
 
 apt-get update && apt-get install -y curl memcached haveged apt-transport-https
 apt-get update && apt-get install -y wget lsb-release gnupg2
-wget -O - https://files.freeswitch.org/repo/deb/debian-release/fsstretch-archive-keyring.asc | apt-key add -
-echo "deb http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
-echo "deb-src http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+
+if [ ."$cpu_architecture" = ."x86" ]; then
+	wget -O - https://files.freeswitch.org/repo/deb/debian-release/fsstretch-archive-keyring.asc | apt-key add -
+	echo "deb http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+	echo "deb-src http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+fi
+if [ ."$cpu_architecture" = ."arm" ]; then
+	wget -O - https://files.freeswitch.org/repo/deb/rpi/debian-release/freeswitch_archive_g0.pub | apt-key add -
+	echo "deb http://files.freeswitch.org/repo/deb/rpi/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+	echo "deb-src http://files.freeswitch.org/repo/deb/rpi/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+fi
 
 apt-get update
 apt-get install -y gdb ntp
