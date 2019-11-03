0eb5e092c74cd36f95b533136cf4335863400b0b
FusionPBX <markjcrane@gmail.com>
2019-11-03 10:31:13 -0700

    Update config.sh

--- ubuntu/resources/config.sh
+++ ubuntu/resources/config.sh
@@ -3,7 +3,7 @@
 domain_name=ip_address          # hostname, ip_address or a custom value
 system_username=admin           # default username admin
 system_password=random          # random or a custom value
-system_branch=stable            # master, stable
+system_branch=master            # master, stable
 
 # FreeSWITCH Settings
 switch_branch=stable            # master, stable
