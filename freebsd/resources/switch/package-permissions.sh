0f09e9b1aae1930a8a8e37af3bc122a6ec7cc9a5
FusionPBX <markjcrane@gmail.com>
2019-11-28 05:00:59 -0700

    Update package-permissions.sh

--- freebsd/resources/switch/package-permissions.sh
+++ freebsd/resources/switch/package-permissions.sh
@@ -9,3 +9,4 @@ chown -R www:www /var/lib/freeswitch
 chown -R www:www /usr/local/share/freeswitch
 chown -R www:www /var/log/freeswitch
 chown -R www:www /var/run/freeswitch
+chmod -R 554 /usr/local/share/freeswitch/scripts
