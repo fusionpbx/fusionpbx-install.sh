a0280f5bf0d12e574c0d7f600d481450859b8c7f
FusionPBX <markjcrane@gmail.com>
2019-11-28 03:42:39 -0700

    Update fusionpbx.sh

--- centos/resources/fusionpbx.sh
+++ centos/resources/fusionpbx.sh
@@ -37,6 +37,8 @@ chown -R freeswitch:daemon /var/cache/fusionpbx
 
 #get the source code
 git clone $BRANCH https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+chmod -R 554 /var/www/fusionpbx
+chmod -R 554 /var/www/fusionpbx/secure
 
 #send a message
 verbose "FusionPBX Installed"
