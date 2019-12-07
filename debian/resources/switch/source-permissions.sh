3c584246111c08d9113bc79da02e9e51b621ba87
FusionPBX <markjcrane@gmail.com>
2019-12-06 23:45:17 -0700

    Update source-permissions.sh

--- debian/resources/switch/source-permissions.sh
+++ debian/resources/switch/source-permissions.sh
@@ -3,4 +3,3 @@ chmod -R ug+rw /usr/local/freeswitch
 touch /usr/local/freeswitch/freeswitch.log
 chown -R www-data:www-data /usr/local/freeswitch
 find /usr/local/freeswitch -type d -exec chmod 2770 {} \;
-chmod -R 554 /usr/local/freeswitch/scripts
