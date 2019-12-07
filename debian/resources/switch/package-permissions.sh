7c75272e984751c242863a8ad90068283d706368
FusionPBX <markjcrane@gmail.com>
2019-12-06 23:40:30 -0700

    Update package-permissions.sh

--- debian/resources/switch/package-permissions.sh
+++ debian/resources/switch/package-permissions.sh
@@ -5,4 +5,3 @@ chown -R www-data:www-data /usr/share/freeswitch
 chown -R www-data:www-data /var/log/freeswitch
 chown -R www-data:www-data /var/run/freeswitch
 chown -R www-data:www-data /var/cache/fusionpbx
-chmod -R 554 /usr/share/freeswitch/scripts
