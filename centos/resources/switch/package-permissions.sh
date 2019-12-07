c1320d9365912fe293f2213f88dddcafaeaa410c
FusionPBX <markjcrane@gmail.com>
2019-12-06 23:43:54 -0700

    Update package-permissions.sh

--- centos/resources/switch/package-permissions.sh
+++ centos/resources/switch/package-permissions.sh
@@ -6,4 +6,3 @@ chown -R freeswitch:daemon /var/lib/freeswitch
 chown -R freeswitch:daemon /usr/share/freeswitch
 chown -R freeswitch:daemon /var/log/freeswitch
 chown -R freeswitch:daemon /var/run/freeswitch
-chmod -R 554 /usr/share/freeswitch/scripts
