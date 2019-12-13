084a15b3c90d9385d0f5925a95881df53bab9094
chansizzle <14916599+chansizzle@users.noreply.github.com>
2019-12-13 14:57:41 -0700

    Update source-release.sh

--- ubuntu/resources/switch/source-release.sh
+++ ubuntu/resources/switch/source-release.sh
@@ -24,8 +24,12 @@ CWD=$(pwd)
 echo "Using version $switch_version"
 cd /usr/src
 #git clone -b v1.8 https://freeswitch.org/stash/scm/fs/freeswitch.git /usr/src/freeswitch
-wget http://files.freeswitch.org/freeswitch-releases/freeswitch-$switch_version.zip
-unzip freeswitch-$switch_version.zip
+#1.8 and older
+#wget http://files.freeswitch.org/freeswitch-releases/freeswitch-$switch_version.zip
+#unzip freeswitch-$switch_version.zip
+#1.10.0 and newer
+wget http://files.freeswitch.org/freeswitch-releases/freeswitch-$switch_version.-release.zip
+unzip freeswitch-$switch_version.-release.zip
 rm -R freeswitch
 mv freeswitch-$switch_version freeswitch
 cd /usr/src/freeswitch
