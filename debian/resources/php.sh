29a8a40040591ddde47641a7f34803fff6f6aa5c
FusionPBX <markjcrane@gmail.com>
2020-03-28 17:55:50 -0600

    Update php.sh

--- debian/resources/php.sh
+++ debian/resources/php.sh
@@ -91,7 +91,7 @@ if [ ."$php_version" = ."7.3" ]; then
 fi
 sed 's#post_max_size = .*#post_max_size = 80M#g' -i $php_ini_file
 sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i $php_ini_file
-sed 's#; max_input_vars = .*#max_input_vars = 8000#g' -i $php_ini_file
+sed 's#;max_input_vars = .*#max_input_vars = 8000#g' -i $php_ini_file
 
 #install ioncube
 if [ .$cpu_architecture = .'x86' ]; then
