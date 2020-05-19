4cc678face1b1c5dc10b583ae03277faf107d6a8
FusionPBX <markjcrane@gmail.com>
2020-05-18 22:32:18 -0600

    install dependencies that depend on the operating system version

--- debian/resources/switch/source-release.sh
+++ debian/resources/switch/source-release.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 
 #includes
 . ../config.sh
+. ../environment.sh
 
 #upgrade packages
 apt update && apt upgrade -y
@@ -14,7 +15,15 @@ apt install -y autoconf automake devscripts g++ git-core libncurses5-dev libtool
 apt install -y pkg-config flac  libgdbm-dev libdb-dev gettext sudo equivs mlocate git dpkg-dev libpq-dev
 apt install -y liblua5.2-dev libtiff5-dev libperl-dev libcurl4-openssl-dev libsqlite3-dev libpcre3-dev
 apt install -y devscripts libspeexdsp-dev libspeex-dev libldns-dev libedit-dev libopus-dev libmemcached-dev
-apt install -y libshout3-dev libmpg123-dev libmp3lame-dev yasm nasm libsndfile1-dev libuv1-dev libvpx-dev libvpx4
+apt install -y libshout3-dev libmpg123-dev libmp3lame-dev yasm nasm libsndfile1-dev libuv1-dev libvpx-dev
+
+#install dependencies that depend on the operating system version
+if [ ."$os_codename" = ."stretch" ]; then
+	apt install -y libvpx4
+fi
+if [ ."$os_codename" = ."buster" ]; then
+	apt install -y libvpx5
+fi
 
 # additional dependencies
 apt install -y sqlite3 swig3.0 unzip
