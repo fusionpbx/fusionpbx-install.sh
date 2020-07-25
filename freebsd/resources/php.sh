5d3a0d2624281b07231af208029831bacccab8a3
FusionPBX <markjcrane@gmail.com>
2020-07-25 09:29:40 -0600

    Add php filter

--- freebsd/resources/php.sh
+++ freebsd/resources/php.sh
@@ -14,13 +14,13 @@ echo "DEFAULT_VERSIONS+=ssl=openssl" >> /etc/make.conf
 #install dependencies
 if [ ."$php_version" = ."5" ]; then
 	pkg install --yes php56 php56-phar php56-pgsql php56-pdo php56-pdo_pgsql php56-pdo_odbc php56-pdo_sqlite php56-json php56-gd
-	pkg install --yes php56-imap php56-ldap php56-openssl php56-sockets php56-simplexml php56-xml php56-session
+	pkg install --yes php56-imap php56-ldap php56-openssl php56-sockets php56-simplexml php56-xml php56-session php56-filter
 fi
 if [ ."$php_version" = ."7.1" ]; then
 	#pkg install --yes php70 php70-phar php70-pgsql php70-pdo php70-pdo_pgsql php70-pdo_odbc php70-pdo_sqlite php70-json php70-gd 
 	#pkg install --yes php70-imap php70-ldap php70-openssl php70-sockets php70-simplexml php70-xml php70-session
 	pkg install --yes php71 php71-phar php71-pdo php71-pdo_odbc php71-pdo_sqlite php71-json php71-gd php71-imap
-	pkg install --yes php71-ldap php71-openssl php71-sockets php71-simplexml php71-xml php71-session
+	pkg install --yes php71-ldap php71-openssl php71-sockets php71-simplexml php71-xml php71-session php71-filter
 	if [ ."$database_version" = ."9.3" ]; then
 		pkg install --yes postgresql93-client
 		pkg install --yes php71-pdo_pgsql php71-pgsql
@@ -48,7 +48,7 @@ fi
 if [ ."$php_version" = ."7.2" ]; then
 	pkg install --yes php72 php72-phar php72-pdo php72-pdo_odbc php72-pdo_sqlite php72-json php72-gd php72-imap
 	pkg install --yes php72-ldap php72-openssl php72-sockets php72-simplexml php72-xml php72-session php72-iconv
-	pkg intsall --yes php72-zlib 
+	pkg intsall --yes php72-zlib php72-filter
 	if [ ."$database_version" = ."9.5" ]; then
 		pkg install --yes postgresql95-client
 		pkg install --yes php72-pdo_pgsql php72-pgsql
@@ -68,7 +68,7 @@ fi
 if [ ."$php_version" = ."7.3" ]; then
 	pkg install --yes php73 php73-phar php73-pdo php73-pdo_odbc php73-pdo_sqlite php73-json php73-gd php73-imap
 	pkg install --yes php73-ldap php73-openssl php73-sockets php73-simplexml php73-xml php73-session php73-iconv
-	pkg intsall --yes php73-zlib
+	pkg intsall --yes php73-zlib php73-filter
 	if [ ."$database_version" = ."11" ]; then
 		pkg install --yes postgresql11-client
 		pkg install --yes php73-pdo_pgsql php73-pgsql
