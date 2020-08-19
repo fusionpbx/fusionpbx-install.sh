9b40c12126b8031927325c5cedf7fd554a33b6a8
FusionPBX <markjcrane@gmail.com>
2020-08-19 13:23:09 -0600

    Update postgresql.sh

--- debian/resources/postgresql.sh
+++ debian/resources/postgresql.sh
@@ -19,7 +19,11 @@ echo "Install PostgreSQL and create the database and users\n"
 
 #included in the distribution
 if [ ."$database_repo" = ."system" ]; then
-	apt-get install -y sudo postgresql
+	if [ ."$database_host" != "127.0.0.1" && ."$database_host" != "::1"] ; then
+		apt-get install -y sudo postgresql
+	else
+		apt-get install -y sudo postgresql-client
+	fi
 fi
 
 #postgres official repository
@@ -38,31 +42,39 @@ if [ ."$database_repo" = ."official" ]; then
 		echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" > /etc/apt/sources.list.d/postgresql.list
 		wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 	fi
-	if [ ."$database_version" = ."latest" ]; then
-                apt-get install -y sudo postgresql
+	if [ ."$database_host" != "127.0.0.1" && ."$database_host" != "::1"] ; then
+		if [ ."$database_version" = ."latest" ]; then
+			apt-get install -y sudo postgresql
+		fi
+		if [ ."$database_version" = ."9.6" ]; then
+			apt-get install -y sudo postgresql-$database_version
+		fi
+		if [ ."$database_version" = ."9.4" ]; then
+			apt-get install -y sudo postgresql-$database_version
+		fi
+	else
+		apt-get install -y sudo postgresql-client
 	fi
-	if [ ."$database_version" = ."9.6" ]; then
-                apt-get install -y sudo postgresql-$database_version
-        fi
-	if [ ."$database_version" = ."9.4" ]; then
-                apt-get install -y sudo postgresql-$database_version
-        fi
 fi
 
 #add PostgreSQL and 2ndquadrant repos
 if [ ."$database_repo" = ."2ndquadrant" ]; then
-	apt install -y curl
-	curl https://dl.2ndquadrant.com/default/release/get/deb | bash
-        if [ ."$os_codename" = ."buster" ]; then
-                sed -i /etc/apt/sources.list.d/2ndquadrant-dl-default-release.list -e 's#buster#stretch#g'
-        fi
-        apt update
-	apt-get install -y sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4
+	if [ ."$database_host" != "127.0.0.1" && ."$database_host" != "::1"] ; then
+		apt install -y curl
+		curl https://dl.2ndquadrant.com/default/release/get/deb | bash
+		if [ ."$os_codename" = ."buster" ]; then
+			sed -i /etc/apt/sources.list.d/2ndquadrant-dl-default-release.list -e 's#buster#stretch#g'
+		fi
+		apt update
+		apt-get install -y sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4	
+	fi
 fi
 
 #systemd
-systemctl daemon-reload
-systemctl restart postgresql
+if [ ."$database_host" != "127.0.0.1" && ."$database_host" != "::1"] ; then
+	systemctl daemon-reload
+	systemctl restart postgresql
+fi
 
 #init.d
 #/usr/sbin/service postgresql restart
@@ -79,15 +91,18 @@ systemctl restart postgresql
 cwd=$(pwd)
 cd /tmp
 
-#add the databases, users and grant permissions to them
-sudo -u postgres psql -c "CREATE DATABASE fusionpbx;";
-sudo -u postgres psql -c "CREATE DATABASE freeswitch;";
-sudo -u postgres psql -c "CREATE ROLE fusionpbx WITH SUPERUSER LOGIN PASSWORD '$password';"
-sudo -u postgres psql -c "CREATE ROLE freeswitch WITH SUPERUSER LOGIN PASSWORD '$password';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE fusionpbx to fusionpbx;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to fusionpbx;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to freeswitch;"
-#ALTER USER fusionpbx WITH PASSWORD 'newpassword';
+if [ ."$database_host" != "127.0.0.1" && ."$database_host" != "::1"] ; then
+	# add the databases, users and grant permissions to them
+	sudo -u postgres psql -c "CREATE DATABASE fusionpbx;";
+	sudo -u postgres psql -c "CREATE DATABASE freeswitch;";
+	sudo -u postgres psql -c "CREATE ROLE fusionpbx WITH SUPERUSER LOGIN PASSWORD '$password';"
+	sudo -u postgres psql -c "CREATE ROLE freeswitch WITH SUPERUSER LOGIN PASSWORD '$password';"
+	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE fusionpbx to fusionpbx;"
+	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to fusionpbx;"
+	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to freeswitch;"
+	# ALTER USER fusionpbx WITH PASSWORD 'newpassword';
+fi
+
 cd $cwd
 
 #set the ip address
