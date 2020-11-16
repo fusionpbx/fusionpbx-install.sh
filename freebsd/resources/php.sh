fd6d398c5e58a8b727562e06e9751fdeb80579cf
FusionPBX <markjcrane@gmail.com>
2020-11-15 19:32:19 -0700

    Update php.sh
    Add php74 and pdo_pgsql out of the database_version if statement.

--- freebsd/resources/php.sh
+++ freebsd/resources/php.sh
@@ -21,9 +21,9 @@ if [ ."$php_version" = ."7.1" ]; then
 	#pkg install --yes php70-imap php70-ldap php70-openssl php70-sockets php70-simplexml php70-xml php70-session
 	pkg install --yes php71 php71-phar php71-pdo php71-pdo_odbc php71-pdo_sqlite php71-json php71-gd php71-imap
 	pkg install --yes php71-ldap php71-openssl php71-sockets php71-simplexml php71-xml php71-session php71-filter
+	pkg install --yes php71-pdo_pgsql php71-pgsql
 	if [ ."$database_version" = ."9.3" ]; then
 		pkg install --yes postgresql93-client
-		pkg install --yes php71-pdo_pgsql php71-pgsql
 	fi
 	if [ ."$database_version" = ."9.6" ]; then
 		pkg install --yes postgresql96-client
@@ -48,10 +48,9 @@ fi
 if [ ."$php_version" = ."7.2" ]; then
 	pkg install --yes php72 php72-phar php72-pdo php72-pdo_odbc php72-pdo_sqlite php72-json php72-gd php72-imap
 	pkg install --yes php72-ldap php72-openssl php72-sockets php72-simplexml php72-xml php72-session php72-iconv
-	pkg intsall --yes php72-zlib php72-filter
+	pkg intsall --yes php72-zlib php72-filter php72-pdo_pgsql php72-pgsql
 	if [ ."$database_version" = ."9.5" ]; then
 		pkg install --yes postgresql95-client
-		pkg install --yes php72-pdo_pgsql php72-pgsql
 	fi
 	if [ ."$database_version" = ."10" ]; then
 		pkg install --yes postgresql10-client
@@ -68,10 +67,17 @@ fi
 if [ ."$php_version" = ."7.3" ]; then
 	pkg install --yes php73 php73-phar php73-pdo php73-pdo_odbc php73-pdo_sqlite php73-json php73-gd php73-imap
 	pkg install --yes php73-ldap php73-openssl php73-sockets php73-simplexml php73-xml php73-session php73-iconv
-	pkg intsall --yes php73-zlib php73-filter
+	pkg intsall --yes php73-zlib php73-filter php73-pdo_pgsql php73-pgsql
 	if [ ."$database_version" = ."11" ]; then
 		pkg install --yes postgresql11-client
-		pkg install --yes php73-pdo_pgsql php73-pgsql
+	fi
+fi
+if [ ."$php_version" = ."7.4" ]; then
+	pkg install --yes php74 php74-phar php74-pdo php74-pdo_odbc php74-pdo_sqlite php74-json php74-gd php74-imap
+	pkg install --yes php74-ldap php74-openssl php74-sockets php74-simplexml php74-xml php74-session php74-iconv
+	pkg intsall --yes php74-zlib php74-filter php74-pdo_pgsql php74-pgsql
+	if [ ."$database_version" = ."12" ]; then
+		pkg install --yes postgresql12-client
 	fi
 fi
 #send a message
