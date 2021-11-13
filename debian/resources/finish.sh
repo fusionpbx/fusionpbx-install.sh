1e3c3e914a0b4f9c58b2b2ec16330ce14c208a1f
FusionPBX <markjcrane@gmail.com>
2021-11-13 14:09:06 -0700

    Update finish.sh

--- debian/resources/finish.sh
+++ debian/resources/finish.sh
@@ -100,8 +100,6 @@ cd /var/www/fusionpbx && php /var/www/fusionpbx/core/upgrade/upgrade_domains.php
 #restart freeswitch
 /bin/systemctl daemon-reload
 /bin/systemctl restart freeswitch
-/bin/systemctl restart php${php_version}-fpm
-
 
 #welcome message
 echo ""
