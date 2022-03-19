13937e5a8a8aa738ee076195e8a296e235004f45
FusionPBX <markjcrane@gmail.com>
2022-03-19 09:43:00 -0600

    Improve the descriptions for the config.sh file.

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -1,29 +1,29 @@
 
 # FusionPBX Settings
-domain_name=ip_address          # hostname, ip_address or a custom value
-system_username=admin           # default username admin
-system_password=random          # random or a custom value
-system_branch=master            # master, stable
+domain_name=ip_address                      # hostname, ip_address or a custom value
+system_username=admin                       # default username admin
+system_password=random                      # random or a custom value
+system_branch=master                        # master, stable
 
 # FreeSWITCH Settings
-switch_branch=stable            # master, stable
-switch_source=false             # true or false
-switch_package=true             # true or false
-switch_version=1.10.3           # only for source
-switch_tls=true                 # true or false
-switch_token=                   # Personal Auth Token from https://signalwire.com
-
+switch_branch=stable                        # master, stable
+switch_source=true                          # true (source compile) or false (binary package)
+switch_package=false                        # true (binary package) or false (source compile)
+switch_version=1.10.3                       # which source code to download, only for source
+switch_tls=true                             # true or false
+switch_token=                               # Get the auth token from https://signalwire.com
+                                            # Signup or Login -> Profile -> Personal Auth Token
 # Sofia-Sip Settings
-sofia_version=1.13.6            # release-version for sofia-sip to use
+sofia_version=1.13.6                        # release-version for sofia-sip to use
 
 # Database Settings
-database_password=random        # random or a custom value (safe characters A-Z, a-z, 0-9)
-database_repo=official          # PostgreSQL official, system, 2ndquadrant
-database_version=latest         # requires repo official
-database_host=127.0.0.1         # hostname or IP address
-database_port=5432              # port number
-database_backup=false           # true or false
+database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
+database_repo=official                      # PostgreSQL official, system, 2ndquadrant
+database_version=latest                     # requires repo official
+database_host=127.0.0.1                     # hostname or IP address
+database_port=5432                          # port number
+database_backup=false                       # true or false
 
 # General Settings
-php_version=7.1                 # PHP version 7.1, 7.3, 7.4
-letsencrypt_folder=true         # true or false
+php_version=7.4                             # PHP version 7.1, 7.3, 7.4
+letsencrypt_folder=true                     # true or false
