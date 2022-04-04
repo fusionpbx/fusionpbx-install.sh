8dbb543d72a2140bd008babfafe476a1e40fa041
Valentin Kleibel <valentin@vrvis.at>
2022-04-04 16:20:22 +0200

    devuan: merge nginx changes from debian

--- devuan/resources/nginx.sh
+++ devuan/resources/nginx.sh
@@ -9,69 +9,36 @@ cd "$(dirname "$0")"
 . ./environment.sh
 
 #send a message
-verbose "Installing Nginx"
+verbose "Installing the web server"
 
-#if [ ."$cpu_architecture" = ."arm" ]; then
-        #9.x - */stretch/
-        #8.x - */jessie/
-#fi
-if [ ."$php_version" = ."5" ]; then
-        #verbose "Switching forcefully to php5* packages"
-        which add-apt-repository || apt-get install -y software-properties-common
-        #LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-        #LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php5-compat
-        apt-get update
-elif [ ."$os_name" = ."Ubuntu" ]; then
-        #16.10.x - */yakkety/
-        #16.04.x - */xenial/
-        #14.04.x - */trusty/
-        if [ ."$os_codename" = ."trusty" ]; then
-                which add-apt-repository || apt-get install -y software-properties-common
-                LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-                apt-get -q update
-        fi
-elif [ ."$cpu_architecture" = ."arm" ]; then
-        #Pi2 and Pi3 Raspbian
-        #Odroid
-        if [ ."$os_codename" = ."jessie" ]; then
-                echo "deb http://packages.moopi.uk/debian jessie main" > /etc/apt/sources.list.d/moopi.list
-                wget -O - http://packages.moopi.uk/debian/moopi.gpg.key | apt-key add -
-                apt-get -q update
-        fi
-else
-        #9.x - */stretch/
-        #8.x - */jessie/
-        if [ ."$os_codename" = ."jessie" ]; then
-                echo "deb http://packages.dotdeb.org $os_codename all" > /etc/apt/sources.list.d/dotdeb.list
-                echo "deb-src http://packages.dotdeb.org $os_codename all" >> /etc/apt/sources.list.d/dotdeb.list
-                wget -O - https://www.dotdeb.org/dotdeb.gpg | apt-key add -
-                apt-get -q update
-        fi
+#change the version of php for arm
+if [ ."$cpu_architecture" = ."arm" ]; then
+	#Pi2 and Pi3 Raspbian
+	#Odroid
+	if [ ."$os_codename" = ."stretch" ]; then
+	      php_version=7.2
+	else
+	      php_version=5.6
+	fi
 fi
 
-#use php version 5 for arm
-#if [ .$cpu_architecture = .'arm' ]; then
-#        php_version=5
-#fi
-
-#install dependencies
-apt-get install -y -q nginx
-if [ ."$php_version" = ."5" ]; then
-        apt-get install -y -q php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap
+#set the version of php
+if [ ."$os_codename" = ."chimaera" ]; then
+	php_version=7.4
 fi
-if [ ."$php_version" = ."7" ]; then
-        apt-get install -y -q php7.0 php7.0-cli php7.0-fpm php7.0-pgsql php7.0-sqlite3 php7.0-odbc php7.0-curl php7.0-imap php7.0-xml
+if [ ."$os_codename" = ."beowulf" ]; then
+	php_version=7.3
 fi
 
 #enable fusionpbx nginx config
 cp nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
 
 #prepare socket name
-if [ ."$php_version" = ."5" ]; then
-        sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php5-fpm.sock;#g'
+if [ ."$php_version" = ."7.3" ]; then
+        sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.3-fpm.sock;#g'
 fi
-if [ ."$php_version" = ."7" ]; then
-        sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.0-fpm.sock;#g'
+if [ ."$php_version" = ."7.4" ]; then
+        sed -i /etc/nginx/sites-available/fusionpbx -e 's#unix:.*;#unix:/var/run/php/php7.4-fpm.sock;#g'
 fi
 ln -s /etc/nginx/sites-available/fusionpbx /etc/nginx/sites-enabled/fusionpbx
 
@@ -82,8 +49,15 @@ ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/ssl/certs/nginx.crt
 #remove the default site
 rm /etc/nginx/sites-enabled/default
 
+#update config if LetsEncrypt folder is unwanted
+# if [ .$letsencrypt_folder = .false ]; then
+#         sed -i '151,155d' /etc/nginx/sites-available/fusionpbx
+# fi
+
 #add the letsencrypt directory
-mkdir -p /var/www/letsencrypt/
+if [ .$letsencrypt_folder = .true ]; then
+        mkdir -p /var/www/letsencrypt/
+fi
 
 #restart nginx
-service nginx restart
+/usr/sbin/service nginx restart
