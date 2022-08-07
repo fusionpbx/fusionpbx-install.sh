ccf6adce5a728a4fb6fc9d82e6291ba3550b4ea5
Scuzz12 <37719501+Scuzz12@users.noreply.github.com>
2022-08-07 15:00:13 -0400

    Update pre-install.sh

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -7,7 +7,8 @@ apt-get update && apt-get upgrade -y
 apt-get install -y git lsb-release
 
 #get the install script
-cd /usr/src && git clone https://github.com/Scuzz12/fusionpbx-install.sh.git
+cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
 
 #change the working directory
 cd /usr/src/fusionpbx-install.sh/debian
+Footer
