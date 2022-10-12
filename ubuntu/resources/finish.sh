2c86a8db1c5a3c0cf75e7e7da1eeedc34351d556
FusionPBX <markjcrane@gmail.com>
2022-10-12 09:39:21 -0600

    Remove config.php and chown

--- ubuntu/resources/finish.sh
+++ ubuntu/resources/finish.sh
@@ -30,17 +30,8 @@ chmod 755 /etc/cron.daily/fusionpbx-maintenance
 sed -i "s/zzz/$database_password/g" /etc/cron.daily/fusionpbx-backup
 sed -i "s/zzz/$database_password/g" /etc/cron.daily/fusionpbx-maintenance
 
-#add the config.php
-mkdir -p /etc/fusionpbx
-chown -R www-data:www-data /etc/fusionpbx
-cp fusionpbx/config.php /etc/fusionpbx
-sed -i /etc/fusionpbx/config.php -e s:"{database_host}:$database_host:"
-sed -i /etc/fusionpbx/config.php -e s:'{database_username}:fusionpbx:'
-sed -i /etc/fusionpbx/config.php -e s:"{database_password}:$database_password:"
-
 #add the config.conf
 mkdir -p /etc/fusionpbx
-chown -R www-data:www-data /etc/fusionpbx
 cp fusionpbx/config.conf /etc/fusionpbx
 sed -i /etc/fusionpbx/config.conf -e s:"{database_host}:$database_host:"
 sed -i /etc/fusionpbx/config.conf -e s:'{database_username}:fusionpbx:'
