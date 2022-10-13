1887bef722824518a091145c27f5a142683b3165
FusionPBX <markjcrane@gmail.com>
2022-10-13 15:11:13 -0600

    Add additional database settings.

--- centos/resources/config.sh
+++ centos/resources/config.sh
@@ -10,4 +10,10 @@ switch_source=false             # true or false
 switch_package=true             # true or false
 
 # Database Settings
-database_password=random        # random or as a pre-set value
+database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
+database_username=fusionpbx                 # Database username (safe characters A-Z, a-z, 0-9)
+database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
+database_repo=official                      # PostgreSQL official, system, 2ndquadrant
+database_host=127.0.0.1                     # hostname or IP address
+database_port=5432                          # port number
+database_backup=false                       # true or false
