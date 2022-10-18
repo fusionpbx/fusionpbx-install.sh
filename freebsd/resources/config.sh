ecfba00d18a5e995d0663ab0418d47372debadd8
FusionPBX <markjcrane@gmail.com>
2022-10-17 23:04:40 -0600

    Update config.sh

--- freebsd/resources/config.sh
+++ freebsd/resources/config.sh
@@ -12,8 +12,10 @@ switch_source=package           # package, port, source
 switch_tls=true                 # true or false
 
 # Database Settings
+database_name=fusionpbx         # Database name (safe characters A-Z, a-z, 0-9)
+database_username=fusionpbx     # Database username (safe characters A-Z, a-z, 0-9)
+database_password=random        # random or a custom value (safe characters A-Z, a-z, 0-9)
 database_enabled=true           # true or false
-database_password=random        # random or as a pre-set value
 database_version=13             # Postgres 14, 13, 12, 11, 10, 9.6
 database_host=127.0.0.1         # hostname or IP address
 database_port=5432              # port number
