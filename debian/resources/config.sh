5ff0e1493c8c0a7900375611bae96d056d135b24
FusionPBX <markjcrane@gmail.com>
2022-10-19 20:37:16 -0600

    Chang version to 1.10.8

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -9,7 +9,7 @@ system_branch=master                        # master, stable
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.7                       # which source code to download, only for source
+switch_version=1.10.8                       # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
