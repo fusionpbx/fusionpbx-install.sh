bd3e11e5a9922ec4a4af60024e946438499e7cef
FusionPBX <markjcrane@gmail.com>
2023-02-07 11:10:24 -0700

    Update version to FreeSWITCH 1.10.9 and Sofia 1.13.13

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -9,12 +9,12 @@ system_branch=master                        # master, stable
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.8                       # which source code to download, only for source
+switch_version=1.10.9                       # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
 # Sofia-Sip Settings
-sofia_version=1.13.8                        # release-version for sofia-sip to use
+sofia_version=1.13.13                        # release-version for sofia-sip to use
 
 # Database Settings
 database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
