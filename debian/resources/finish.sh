6d9bdb58af98e18d84bd3356ee4a5420920e434e
FusionPBX <markjcrane@gmail.com>
2023-05-10 18:47:31 -0600

    Update finish.sh

--- debian/resources/finish.sh
+++ debian/resources/finish.sh
@@ -38,8 +38,10 @@ sed -i /etc/fusionpbx/config.conf -e s:"{database_name}:$database_name:"
 sed -i /etc/fusionpbx/config.conf -e s:"{database_username}:$database_username:"
 sed -i /etc/fusionpbx/config.conf -e s:"{database_password}:$database_password:"
 
+sleep 1
+
 #add the database schema
-cd /var/www/fusionpbx && php /var/www/fusionpbx/core/upgrade/upgrade_schema.php > /dev/null 2>&1
+cd /var/www/fusionpbx && php /var/www/fusionpbx/core/upgrade/upgrade_schema.php
 
 #get the server hostname
 if [ .$domain_name = .'hostname' ]; then
@@ -58,7 +60,7 @@ domain_uuid=$(/usr/bin/php /var/www/fusionpbx/resources/uuid.php);
 psql --host=$database_host --port=$database_port --username=$database_username -c "insert into v_domains (domain_uuid, domain_name, domain_enabled) values('$domain_uuid', '$domain_name', 'true');"
 
 #app defaults
-cd /var/www/fusionpbx && php /var/www/fusionpbx/core/upgrade/upgrade_domains.php
+cd /var/www/fusionpbx && /usr/bin/php /var/www/fusionpbx/core/upgrade/upgrade_domains.php
 
 #add the user
 user_uuid=$(/usr/bin/php /var/www/fusionpbx/resources/uuid.php);
@@ -69,7 +71,7 @@ if [ .$system_password = .'random' ]; then
 else
 	user_password=$system_password
 fi
-password_hash=$(php -r "echo md5('$user_salt$user_password');");
+password_hash=$(/usr/bin/php -r "echo md5('$user_salt$user_password');");
 psql --host=$database_host --port=$database_port --username=$database_username -t -c "insert into v_users (user_uuid, domain_uuid, username, password, salt, user_enabled) values('$user_uuid', '$domain_uuid', '$user_name', '$password_hash', '$user_salt', 'true');"
 
 #get the superadmin group_uuid
@@ -92,7 +94,7 @@ sed -i /etc/freeswitch/autoload_configs/xml_cdr.conf.xml -e s:"{v_user}:$xml_cdr
 sed -i /etc/freeswitch/autoload_configs/xml_cdr.conf.xml -e s:"{v_pass}:$xml_cdr_password:"
 
 #app defaults
-cd /var/www/fusionpbx && php /var/www/fusionpbx/core/upgrade/upgrade.php
+cd /var/www/fusionpbx && /usr/bin/php /var/www/fusionpbx/core/upgrade/upgrade.php
 
 #restart freeswitch
 /bin/systemctl daemon-reload
