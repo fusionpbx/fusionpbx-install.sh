c58007e0f46004da33e3ef831783a0d069b17c62
FusionPBX <markjcrane@gmail.com>
2023-06-16 16:10:25 -0600

    Use sury repo for PHP 8.1 and Debian system for 7.4

--- debian/resources/upgrade/php.sh
+++ debian/resources/upgrade/php.sh
@@ -24,14 +24,14 @@ apt remove -y php8.1 php8.1-cli php8.1-dev php8.1-fpm php8.1-pgsql php8.1-sqlite
 #remove php 8.2
 apt remove -y php8.2 php8.2-cli php8.2-dev php8.2-fpm php8.2-pgsql php8.2-sqlite3 php8.2-odbc php8.2-curl php8.2-imap php8.2-xml php8.2-gd php8.2-mbstring php8.2-ldap
 
-#add a repo for php 7.x
-apt -y install apt-transport-https lsb-release ca-certificates curl wget gnupg2
-wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sury-php-8.x.gpg
-sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-apt-get update
-
 #install php update and set the unix socket
 if [ ."$php_version" = ."8.2" ]; then
+	#add a repo for php 8.x
+	apt -y install apt-transport-https lsb-release ca-certificates curl wget gnupg2
+	wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sury-php-8.x.gpg
+	sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+	apt-get update
+
 	#install php 8.2
 	apt-get install -y php8.2 php8.2-cli php8.2-dev php8.2-fpm php8.2-pgsql php8.2-sqlite3 php8.2-odbc php8.2-curl php8.2-imap php8.2-xml php8.2-gd php8.2-mbstring php8.2-ldap
 
@@ -42,6 +42,12 @@ if [ ."$php_version" = ."8.2" ]; then
 	php_ini_file='/etc/php/8.2/fpm/php.ini'
 fi
 if [ ."$php_version" = ."8.1" ]; then
+	#add a repo for php 7.x
+	apt -y install apt-transport-https lsb-release ca-certificates curl wget gnupg2
+	wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sury-php-8.x.gpg
+	sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+	apt-get update
+
 	#install php 8.1
 	apt-get install -y php8.1 php8.1-cli php8.1-dev php8.1-fpm php8.1-pgsql php8.1-sqlite3 php8.1-odbc php8.1-curl php8.1-imap php8.1-xml php8.1-gd php8.1-mbstring php8.1-ldap
  
@@ -52,6 +58,10 @@ if [ ."$php_version" = ."8.1" ]; then
 	php_ini_file='/etc/php/8.1/fpm/php.ini'
 fi
 if [ ."$php_version" = ."7.4" ]; then
+	#remove the sury PHP repo
+	rm -f -- /etc/apt/sources.list.d/php.list
+ 	apt update
+ 
 	#install php 7.4
 	apt-get install -y php7.4 php7.4-cli php7.4-dev php7.4-fpm php7.4-pgsql php7.4-sqlite3 php7.4-odbc php7.4-curl php7.4-imap php7.4-xml php7.4-gd php7.4-mbstring php7.4-ldap
  
