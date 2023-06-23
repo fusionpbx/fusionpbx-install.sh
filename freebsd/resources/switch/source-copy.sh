853fe7d0169760b4bf09fd12a35442c3d69f1b4d
FusionPBX <markjcrane@gmail.com>
2023-06-23 13:37:31 -0600

    Update source-copy.sh

--- freebsd/resources/switch/source-copy.sh
+++ freebsd/resources/switch/source-copy.sh
@@ -5,7 +5,7 @@ mv /usr/local/freeswitch/conf /usr/local/freeswitch/conf.orig
 
 #copy the default conf directory
 mkdir -p /usr/local/freeswitch/conf
-cp -R /usr/local/www/fusionpbx/resources/templates/conf/* /usr/local/freeswitch/conf
+cp -R /usr/local/www/fusionpbx/app/switch/resources/conf/* /usr/local/freeswitch/conf
 
 #copy the scripts
 cp -R /usr/local/www/fusionpbx/resources/install/scripts /usr/local/freeswitch
