075113ad8a225d17b3136efb9c3f1f71f14e835a
FusionPBX <markjcrane@gmail.com>
2023-06-23 13:35:25 -0600

    Update conf-copy.sh

--- debian/resources/switch/conf-copy.sh
+++ debian/resources/switch/conf-copy.sh
@@ -1,3 +1,3 @@
 mv /etc/freeswitch /etc/freeswitch.orig
 mkdir /etc/freeswitch
-cp -R /var/www/fusionpbx/resources/templates/conf/* /etc/freeswitch
+cp -R /var/www/fusionpbx/app/switch/resources/conf/* /etc/freeswitch
