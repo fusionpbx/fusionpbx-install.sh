156ddccf44ee37cffe40e122cf2b49e201ee571b
FusionPBX <markjcrane@gmail.com>
2023-09-06 17:29:53 -0600

    Update Switch, Sofia and Postgresql version
    Debian 12, Switch 1.10.10, Sofia 1.13.16, and PostgreSQL 15

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -9,19 +9,19 @@ system_branch=master                        # master, stable
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.9                       # which source code to download, only for source
+switch_version=1.10.10                      # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
 # Sofia-Sip Settings
-sofia_version=1.13.13                        # release-version for sofia-sip to use
+sofia_version=1.13.16                        # release-version for sofia-sip to use
 
 # Database Settings
 database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
 database_username=fusionpbx                 # Database username (safe characters A-Z, a-z, 0-9)
 database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
 database_repo=system                        # PostgreSQL official, system
-database_version=13                         # requires repo official
+database_version=15                         # requires repo official
 database_host=127.0.0.1                     # hostname or IP address
 database_port=5432                          # port number
 database_backup=false                       # true or false
