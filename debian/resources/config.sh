49e2fb434d63adf135e1f867521a9796f23425ca
FusionPBX <markjcrane@gmail.com>
2023-09-21 14:58:47 -0600

    Use PostgreSQL 16 by default

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -14,14 +14,14 @@ switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
 # Sofia-Sip Settings
-sofia_version=1.13.16                        # release-version for sofia-sip to use
+sofia_version=1.13.16                       # release-version for sofia-sip to use
 
 # Database Settings
 database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
 database_username=fusionpbx                 # Database username (safe characters A-Z, a-z, 0-9)
 database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
-database_repo=system                        # PostgreSQL official, system
-database_version=15                         # requires repo official
+database_repo=official                      # PostgreSQL official, system
+database_version=16                         # requires repo official
 database_host=127.0.0.1                     # hostname or IP address
 database_port=5432                          # port number
 database_backup=false                       # true or false
