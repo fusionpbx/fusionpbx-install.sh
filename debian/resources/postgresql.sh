7ceb1293e8210062f1b255e14cd14eb02192159b
FusionPBX <markjcrane@gmail.com>
2023-11-20 16:22:51 -0700

    Update permissions on pgdg.gpg file

--- debian/resources/postgresql.sh
+++ debian/resources/postgresql.sh
@@ -30,6 +30,7 @@ fi
 if [ ."$database_repo" = ."official" ]; then
 	sh -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/pgdg.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/pgdg.gpg
+	chmod 644 /etc/apt/trusted.gpg.d/pgdg.gpg
 	apt-get update && apt-get upgrade -y
 	if [ ."$database_host" = ."127.0.0.1" ] || [ ."$database_host" = ."::1" ] ; then
 		if [ ."$database_version" = ."latest" ]; then
