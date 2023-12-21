309dd5fdbd2f99d53a4aab44a87f1dab03b0365d
FusionPBX <markjcrane@gmail.com>
2023-12-21 10:16:47 -0700

    Remove freeswitch database and user
    Reason for this change is if FreeSWITCH uses the PostgreSQL then its a good idea for it to use its own instance. This is to prevent blocking FreeSWITCH from accessing the database if there is a long running Query on the FusionPBX database

--- debian/resources/postgresql/create.sh
+++ debian/resources/postgresql/create.sh
@@ -15,14 +15,10 @@ sudo -u postgres psql -p $database_port -c "SET client_encoding = 'UTF8';";
 
 #add the database users and databases
 sudo -u postgres psql -p $database_port -c "CREATE DATABASE fusionpbx;";
-sudo -u postgres psql -p $database_port -c "CREATE DATABASE freeswitch;";
 
 #add the users and grant permissions
 sudo -u postgres psql -p $database_port -c "CREATE ROLE fusionpbx WITH SUPERUSER LOGIN PASSWORD '$database_password';"
-sudo -u postgres psql -p $database_port -c "CREATE ROLE freeswitch WITH SUPERUSER LOGIN PASSWORD '$database_password';"
 sudo -u postgres psql -p $database_port -c "GRANT ALL PRIVILEGES ON DATABASE fusionpbx to fusionpbx;"
-sudo -u postgres psql -p $database_port -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to fusionpbx;"
-sudo -u postgres psql -p $database_port -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to freeswitch;"
 
 #reload the config
 sudo -u postgres psql -p $database_port -c "SELECT pg_reload_conf();"
