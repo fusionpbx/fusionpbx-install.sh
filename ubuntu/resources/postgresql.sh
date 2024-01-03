9601ff7db8fc102bfa6b707cfbc702cedb848dcc
demonspork <demonspork@gmail.com>
2024-01-03 10:53:16 -0600

    Improve apt key security and always install sngrep from official (#416)
    * Improve apt key security and always install sngrep from official
    
    * Correct PHP GPG key file path

--- ubuntu/resources/postgresql.sh
+++ ubuntu/resources/postgresql.sh
@@ -24,8 +24,8 @@ fi
 
 #postgres official repository
 if [ ."$database_repo" = ."official" ]; then
-	sh -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/pgdg.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/pgdg.gpg
+	sh -c 'echo "deb [signed-by=/etc/apt/keyrings/pgdg.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/keyrings/pgdg.gpg
 	apt-get update && apt-get upgrade -y
 	if [ ."$database_host" = ."127.0.0.1" ] || [ ."$database_host" = ."::1" ] ; then
 		if [ ."$database_version" = ."latest" ]; then
