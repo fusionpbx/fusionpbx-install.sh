5ffcaf4b207937cda1031f7419f554b94883bac6
FusionPBX <markjcrane@gmail.com>
2024-02-03 10:15:06 -0700

    Update FreeSWITCH version to 1.10.11

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -9,7 +9,7 @@ system_branch=5.2                           # master, 5.2
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.10                      # which source code to download, only for source
+switch_version=1.10.11                      # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
