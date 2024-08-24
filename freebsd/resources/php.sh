7bb83997298c087ed0c7128e56715f07548a69f3
FusionPBX <markjcrane@gmail.com>
2024-08-24 11:56:10 -0600

    Update php.sh

--- freebsd/resources/php.sh
+++ freebsd/resources/php.sh
@@ -21,16 +21,19 @@ if [ ."$php_version" = ."7.4" ]; then
 	pkg install --yes php74 php74-phar php74-pdo php74-pdo_odbc php74-pdo_sqlite php74-json php74-gd php74-imap
 	pkg install --yes php74-ldap php74-openssl php74-sockets php74-simplexml php74-xml php74-session php74-iconv
 	pkg install --yes php74-zlib php74-filter php74-pdo_pgsql php74-pgsql php74-curl php74-mbstring
+	pkg install --yes php74-pdo_pgsql php74-pgsql
 fi
 if [ ."$php_version" = ."8.0" ]; then
 	pkg install --yes php80 php80-phar php80-pdo php80-pdo_odbc php80-pdo_sqlite php80-gd php80-imap
 	pkg install --yes php80-ldap php80-sockets php80-simplexml php80-xml php80-session php80-iconv
-	pkg install --yes php80-zlib php80-filter php80-pdo_pgsql php80-pgsql php80-curl php80-mbstring
+	pkg install --yes php80-zlib php80-filter php80-curl php80-mbstring
+	pkg install --yes php80-pdo_pgsql php80-pgsql
 fi
 if [ ."$php_version" = ."8.1" ]; then
 	pkg install --yes php81 php81-phar php81-pdo php81-pdo_odbc php81-pdo_sqlite php81-gd php81-imap
 	pkg install --yes php81-ldap php81-sockets php81-simplexml php81-xml php81-session php81-iconv
-	pkg install --yes php81-zlib php81-filter php81-pdo_pgsql php81-pgsql php81-curl php81-mbstring
+	pkg install --yes php81-zlib php81-filter php81-curl php81-mbstring
+ 	pkg install --yes php81-pdo_pgsql php81-pgsql
 fi
 #send a message
 verbose "Configuring PHP"
