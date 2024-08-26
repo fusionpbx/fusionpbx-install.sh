886d3c17e96c29e29ebfdc160c279acf7af982f1
FusionPBX <markjcrane@gmail.com>
2024-08-25 20:01:59 -0600

    Update pf.conf and settings

--- freebsd/resources/pf.sh
+++ freebsd/resources/pf.sh
@@ -21,5 +21,8 @@ echo 'pflog_logfile="/var/log/pflog"' >> /etc/rc.conf
 cp pf/pf.conf /etc/pf.conf
 
 #add the pf table files
-touch /etc/pf-block.conf
 touch /etc/pf-pass.conf
+touch /etc/pf-block.conf
+touch /etc/pf-block-sip-auth-ip.conf
+touch /etc/pf-block-sip-auth-fail.conf
+
