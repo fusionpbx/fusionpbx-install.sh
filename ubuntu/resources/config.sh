e8b7ae134f4ae120601968bdb211957349d754ff
FusionPBX <markjcrane@gmail.com>
2024-12-17 19:57:25 -0700

    Change the Switch version to 1.10.12

--- ubuntu/resources/config.sh
+++ ubuntu/resources/config.sh
@@ -9,7 +9,7 @@ system_branch=5.3                           # master, 5.3
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.11                      # which source code to download, only for source
+switch_version=1.10.12                      # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
