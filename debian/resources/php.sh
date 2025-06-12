475c7c520373e16185dac669e5da30d5d72aee97
FusionPBX <markjcrane@gmail.com>
2025-06-12 12:03:06 -0600

    Remove PHP 5.6 and 7.0 from the Debian install

--- debian/resources/php.sh
+++ debian/resources/php.sh
@@ -112,12 +112,6 @@ apt-get install -y nginx
 if [ ."$php_version" = ."" ]; then
 	apt-get install -y php php-cli php-fpm php-pgsql php-sqlite3 php-odbc php-curl php-imap php-xml php-gd php-inotify
 fi
-if [ ."$php_version" = ."5.6" ]; then
-	apt-get install -y php5 php5-cli php5-fpm php5-pgsql php5-sqlite php5-odbc php5-curl php5-imap php5-gd
-fi
-if [ ."$php_version" = ."7.0" ]; then
-	apt-get install -y --no-install-recommends php7.0 php7.0-common php7.0-cli php7.0-fpm php7.0-pgsql php7.0-sqlite3 php7.0-odbc php7.0-curl php7.0-imap php7.0-xml php7.0-gd php7.0-mbstring php7.0-inotify
-fi
 if [ ."$php_version" = ."7.1" ]; then
 	apt-get install -y --no-install-recommends php7.1 php7.1-common php7.1-cli php7.1-fpm php7.1-pgsql php7.1-sqlite3 php7.1-odbc php7.1-curl php7.1-imap php7.1-xml php7.1-gd php7.1-mbstring php7.1-inotify
 fi
@@ -144,14 +138,6 @@ if [ ."$php_version" = ."8.4" ]; then
 fi
 
 #update config if source is being used
-if [ ."$php_version" = ."5" ]; then
-        verbose "version 5.x"
-        php_ini_file='/etc/php5/fpm/php.ini'
-fi
-if [ ."$php_version" = ."7.0" ]; then
-        verbose "version 7.0"
-        php_ini_file='/etc/php/7.0/fpm/php.ini'
-fi
 if [ ."$php_version" = ."7.1" ]; then
         verbose "version 7.1"
         php_ini_file='/etc/php/7.1/fpm/php.ini'
@@ -196,12 +182,6 @@ fi
 
 #restart php-fpm
 systemctl daemon-reload
-if [ ."$php_version" = ."5.6" ]; then
-        systemctl restart php5-fpm
-fi
-if [ ."$php_version" = ."7.0" ]; then
-        systemctl restart php7.0-fpm
-fi
 if [ ."$php_version" = ."7.1" ]; then
         systemctl restart php7.1-fpm
 fi
