53edbe73dc57927e4bb19ef3377343eb445d38e2
faberjan <git@fvo.email>
2025-07-21 20:36:25 +0200

    Fix typo in github url

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get upgrade -y
 apt-get install -y git lsb-release
 
 #get the install script
-cd /usr/src && git clone https:/github.com/fusionpbx/fusionpbx-install.sh.git
+cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
 
 #change the working directory
 cd /usr/src/fusionpbx-install.sh/debian
