b0450e753aa9d7b2dcd3956360951ac9cc92eb23
David W Studeman <36393544+dwstudeman@users.noreply.github.com>
2026-02-03 21:45:55 -0800

    Delete debian directory
    Adding modified debian folder

--- debian/resources/random.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-random=$(dd if=/dev/urandom bs=1 count=20 2>/dev/null | base64 | sed 's/[=\+//]//g')
-echo $random
--- debian/resources/switch/conf-copy.sh
+++
@@ -1,3 +0,0 @@
-mv /etc/freeswitch /etc/freeswitch.orig
-mkdir /etc/freeswitch
-cp -R /var/www/fusionpbx/app/switch/resources/conf/* /etc/freeswitch
