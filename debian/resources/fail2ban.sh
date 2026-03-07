fa02fe09650f5dd73443d692605840b940ccaf43
David W Studeman <36393544+dwstudeman@users.noreply.github.com>
2026-03-06 23:27:30 -0800

    Update fail2ban.sh
    An if statement to utilize nftables if installed.

--- debian/resources/fail2ban.sh
+++ debian/resources/fail2ban.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")"
 . ./config.sh
 . ./colors.sh
 . ./environment.sh
+FILE_PATH="/etc/nftables.conf"
 
 #send a message
 verbose "Installing Fail2ban"
@@ -26,10 +27,17 @@ cp fail2ban/fusionpbx-mac.conf /etc/fail2ban/filter.d/fusionpbx-mac.conf
 cp fail2ban/fusionpbx-404.conf /etc/fail2ban/filter.d/fusionpbx-404.conf
 cp fail2ban/nginx-404.conf /etc/fail2ban/filter.d/nginx-404.conf
 cp fail2ban/nginx-dos.conf /etc/fail2ban/filter.d/nginx-dos.conf
-cp fail2ban/jail.local.nfs /etc/fail2ban/jail.local
+if [ -f "$FILE_PATH" ]; then
+    echo "Found nftables to be chosen."
+    cp fail2ban/jail.local.nft /etc/fail2ban/jail.local
+    sed -i 's/iptables/nftables/g' /etc/fail2ban/jail.conf
+    else
+    echo "Default iptables was installed."
+    cp fail2ban/jail.local /etc/fail2ban/jail.local
+fi
 
-#use sed to change iptables to nftables
-sed -i 's/iptables/nftables/g' /etc/fail2ban/jail.conf
+
+
 
 #update config if source is being used
 #if [ .$switch_source = .true ]; then
