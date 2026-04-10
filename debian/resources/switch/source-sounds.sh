2e68b248cb63e5c9bb617c57599360fc98c52007
FusionPBX <markjcrane@gmail.com>
2026-04-10 02:55:00 +0000

    Specify full paths for wget and tar commands
    Make sure wget is installed

--- debian/resources/switch/source-sounds.sh
+++ debian/resources/switch/source-sounds.sh
@@ -10,16 +10,19 @@ cd "$(dirname "$0")"
 # Change the working directory
 cd /usr/share/freeswitch/sounds
 
+# Make sure wget is installed
+apt-get install -y wget
+
 # Array of sample rates
 sample_rates="48000 32000 16000 8000"
 
 # Loop through each sample rate
 for sample_rate in $sample_rates; do
     # Download the file
-    wget "https://files.freeswitch.org/releases/sounds/freeswitch-sounds-en-us-callie-${sample_rate}-1.0.53.tar.gz"
+    /usr/bin/wget "https://files.freeswitch.org/releases/sounds/freeswitch-sounds-en-us-callie-${sample_rate}-1.0.53.tar.gz"
 
     # Extract the file
-    tar xvzf "freeswitch-sounds-en-us-callie-${sample_rate}-1.0.53.tar.gz"
+    /usr/bin/tar xvzf "freeswitch-sounds-en-us-callie-${sample_rate}-1.0.53.tar.gz"
 done
 
 # Change to directory to the music directory
@@ -28,14 +31,14 @@ cd /usr/share/freeswitch/sounds/music
 # Loop through each sample rate
 for sample_rate in $sample_rates; do
     # Download the file
-    wget "https://files.freeswitch.org/releases/sounds/freeswitch-sounds-music-${sample_rate}-1.0.52.tar.gz"
+    /usr/bin/wget "https://files.freeswitch.org/releases/sounds/freeswitch-sounds-music-${sample_rate}-1.0.52.tar.gz"
 
     # Extract the file
-    tar xvzf "freeswitch-sounds-music-${sample_rate}-1.0.52.tar.gz"
+    /usr/bin/tar xvzf "freeswitch-sounds-music-${sample_rate}-1.0.52.tar.gz"
 done
 
 # Move the music to the default directory
-mv music default
+/usr/bin/mv music default
 
 # Remove the tar.gz files
-rm /usr/share/freeswitch/sounds/music/*.tar.gz
+/usr/bin/rm /usr/share/freeswitch/sounds/music/*.tar.gz
