e44dddff69a67ba52b593854f95d221b8cbcf25f
FusionPBX <markjcrane@gmail.com>
2026-09-24 07:48:16 -0600

    Update the install script for FusionPBX 5.6 release - Update nginx config for the single point of entry - Remove nginx rewrite rules built into provision code - nginx rewrite rules are still supported for backwards compatibility

--- freebsd/resources/config.sh
+++ freebsd/resources/config.sh
@@ -3,7 +3,7 @@
 domain_name=hostname            # hostname, ip_address or a custom value
 system_username=admin           # default username admin
 system_password=random          # random or as a pre-set value
-system_branch=5.5               # master, 5.5
+system_branch=5.6               # master, 5.6
 
 # FreeSWITCH Settings
 switch_enabled=true             # true or false
